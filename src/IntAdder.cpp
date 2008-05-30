5a7546f66bdd1d66998588fb7449ef0b48530b74
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2008-05-30 16:53:58 +0000

    IntAdder * State of the art pipelining for IntAdder. NO unused registers, NO xst WARNINGS * All tests prove successful * Correct computation of Pipeline depth
    runsyn
    * modified behaviour when encountering flopoco.vhdl in current folder from:
    	move to project -> copy to project

--- src/IntAdder.cpp
+++ src/IntAdder.cpp
@@ -1,11 +1,10 @@
 /*
  * An integer adder for FloPoCo
-
- It may be pipelined to arbitrary frequency.
- Also useful to derive the carry-propagate delays for the subclasses of Target
-
  *
- * Author : Florent de Dinechin, Bogdan Pasca
+ * It may be pipelined to arbitrary frequency.
+ * Also useful to derive the carry-propagate delays for the subclasses of Target
+ *
+ * Authors : Florent de Dinechin, Bogdan Pasca
  *
  * This file is part of the FloPoCo project developed by the Arenaire
  * team at Ecole Normale Superieure de Lyon
@@ -51,57 +50,71 @@ IntAdder::IntAdder(Target* target, int wIn) :
 	unique_name=name.str();
 
 	// Set up the IO signals
-	add_input ("X", wIn);
-	add_input ("Y", wIn);
-	add_input ("Cin", 1);
-	add_output ("R", wIn);
+	add_input ("X"  , wIn);
+	add_input ("Y"  , wIn);
+	add_input ("Cin", 1  );
+	add_output("R"  , wIn);
 
 	if (target->is_pipelined())
 		set_sequential();
 	else
 		set_combinatorial();
 
-	if (is_sequential()){ 
+	if (is_sequential()){
+		//the maximum chunk size so that (optimisically) the target freqency can be reached. 
 		chunk_size = (int)floor( (1./target->frequency() - target->lut_delay()) / target->carry_propagate_delay()); 
-		pipe_levels = wIn/chunk_size; // =0 when no pipeline
-		if ((pipe_levels==0)||(wIn==chunk_size)){
-			set_pipeline_depth(0); 
+		//the pipe_levels gives th number of chunks that the addition must be split in so that the frequency will ve reached
+		
+		//set the pipeline depth 
+		if (wIn<=chunk_size)
+			pipe_levels=0;
+		else
+			pipe_levels=wIn/chunk_size;
+			
+		set_pipeline_depth(pipe_levels); 
+		if(verbose) {
+			cout << tab <<"Estimated delay will be " << target->adder_delay(wIn) <<endl; 
+			cout << tab << "chunk="<<chunk_size << " freq=" << 1e-6/target->adder_delay(chunk_size) <<" levels="<<pipe_levels <<endl;
 		}
+		if(chunk_size > (wIn/(pipe_levels+1))+2)
+			chunk_size = (wIn/(pipe_levels+1))+2;
+		if(verbose)
+			cout << tab << "after chunk balancing, chunk=="<<chunk_size << " freq=" << 1e-6/target->adder_delay(chunk_size) <<"  levels="<<pipe_levels <<endl;
+		last_chunk_size = wIn - (pipe_levels)*chunk_size;
+		if(verbose)
+			cout << tab << "last chunk=="<<last_chunk_size <<endl;
 
-		else{
-			set_pipeline_depth(pipe_levels); 
-			//  if(c2_pipe_levels) c2_chunk_size=sizeSummand;
-			if(verbose) {
-				cout << tab <<"Estimated delay will be " << target->adder_delay(wIn) <<endl; 
-				cout << tab << "chunk="<<chunk_size << " freq=" << 1e-6/target->adder_delay(chunk_size) <<"  levels="<<pipe_levels <<endl;
-				cout << "****" << (wIn/pipe_levels)+2<< endl;
-			}
-			if(chunk_size > (wIn/pipe_levels)+2)
-				chunk_size = (wIn/pipe_levels)+2;
-			if(verbose)
-				cout << tab << "after chunk balancing, chunk=="<<chunk_size << " freq=" << 1e-6/target->adder_delay(chunk_size) <<"  levels="<<pipe_levels <<endl;
-			last_chunk_size = wIn - (pipe_levels)*chunk_size;
-			if(verbose)
-				cout << tab << "last chunk=="<<last_chunk_size <<endl;
-
+		//if no pipelining is required - then these signals are not present
+		if (pipe_levels>0)
 			for(int i=0; i<=pipe_levels; i++){
-				if (!((i==pipe_levels)&&	(last_chunk_size==0)))
-				{
-					ostringstream snamex, snamey, snamer;
-					snamex <<"ix_"<<i;
-					snamey <<"iy_"<<i;
-					snamer <<"ir_"<<i;
-					int size;
-					if(i<pipe_levels)
-						size = chunk_size+1;
-					else
-						size = last_chunk_size;
-					add_delay_signal_bus(snamex.str(), size, i);
-					add_delay_signal_bus(snamey.str(), size, i);
-					add_delay_signal_bus(snamer.str(), size, pipe_levels-i);
-				}
+				ostringstream snamex, snamey, snamer, cOutR, iSum;
+				int size;
+				
+				snamex <<"ix_"<<i;
+				snamey <<"iy_"<<i;
+				iSum<<"iSum"<<i;	//isum will contain the 1 bit extended sum of ix and iy
+				snamer <<"ir_"<<i;
+				cOutR<<"cOutR"<<i;
+				
+				if(i<pipe_levels)
+					size = chunk_size;
+				else
+					size = last_chunk_size;
+							
+				add_delay_signal_bus(snamex.str(), size, i);
+				add_delay_signal_bus(snamey.str(), size, i);
+				
+				if (i<pipe_levels)	
+					add_signal(iSum.str(),size+1); //is larger because inputs are padded with one 0 each in MSB position
+				else
+					add_signal(iSum.str(),size);
+				
+				add_delay_signal_bus(snamer.str(), size, pipe_levels-i);
+				
+				if (i<pipe_levels)
+					add_registered_signal_with_sync_reset(cOutR.str(), 1);
 			}	
-		}
+		
 	}
 }
 
@@ -131,13 +144,14 @@ void IntAdder::output_vhdl(std::ostream& o, std::string name) {
 
 	o << "begin" << endl;
 	if(is_sequential()){
-		if((pipe_levels==0)||(wIn==chunk_size)){
+		if(pipe_levels==0){
+			//addition simplification
 			o << tab << "R <= X + Y + Cin;" <<endl;      
 		}
 		else{
 			// Initialize the chunks
 			for(int i=0; i<=pipe_levels; i++){
-				if (!((i==pipe_levels)&&	(last_chunk_size==0)))
+				if (!((i==pipe_levels)&&(last_chunk_size==0)))
 				{
 					int maxIndex;
 					if(i==pipe_levels)
@@ -152,49 +166,70 @@ void IntAdder::output_vhdl(std::ostream& o, std::string name) {
 				
 					o << tab << snamex.str() << " <= ";
 					if(i < pipe_levels)
-						o << "\"0\" & ";
+						o << "";
 						o << "X(" << maxIndex<< " downto " << i*chunk_size << ");" << endl;
 				
 					o << tab << snamey.str() << " <= ";
 					if(i < pipe_levels)
-						o << "\"0\" & ";
+						o << "";
 						o << "Y(" << maxIndex<< " downto " << i*chunk_size << ");" << endl;
 				}
 			}
 			// then the pipe_level adders of size chunk_size
 			for(int i=0; i<=pipe_levels; i++){
-				if (!((i==pipe_levels)&&	(last_chunk_size==0)))
+				if (!((i==pipe_levels)&&(last_chunk_size==0)))
 				{
 					int size;
-					if(i==pipe_levels-1) 
-						size=last_chunk_size -1 ;
+					if(i==pipe_levels) 
+						size= last_chunk_size -1 ;
 					else  
 						size = chunk_size;
 					
-					ostringstream snamex, snamey, snamer;
+					ostringstream snamex, snamey, snamer, iSum, cOutR, cOutRM;
 					snamex <<"ix_"<<i;
 					snamey <<"iy_"<<i;
 					snamer <<"ir_"<<i;
+					iSum << "iSum"<<i;
+					cOutR << "cOutR"<<i;
+					cOutRM<< "cOutR"<<i-1;
 					
-					o << tab << snamer.str() << " <= "
-						<< get_delay_signal_name(snamex.str(), i)  
-						<< " + "
-						<< get_delay_signal_name(snamey.str(), i);
+					o<<endl<<endl;
+					
+					o << tab << iSum.str() << " <= ";
+					if(i!=pipe_levels)
+						o<< "(\"0\" & ";
+						o<< get_delay_signal_name(snamex.str(), i) ; 
+					if(i!=pipe_levels)	
+						o<< ")";
+						o<< " + ";
+					if(i!=pipe_levels)	
+						o<< "(\"0\" & ";
+						o<< get_delay_signal_name(snamey.str(), i);
+					if(i!=pipe_levels)	
+						o<< ")";
 					
 					// add the carry in
 					if(i>0) {
 						ostringstream carry;
 						carry <<"ir_"<<i-1;
-						o  << " + " << get_delay_signal_name(carry.str(), 1) << "(" << chunk_size << ")";
+						o  << " + " << get_delay_signal_name(cOutRM.str() , 1);
 					}else
 						o  << " + Cin";
 					
 					o << ";" << endl;
+					
+					if(i<pipe_levels) 
+						o << tab<<snamer.str() <<" <= "<< iSum.str()<<"("<<size-1<<" downto 0);"<<endl;
+					else 
+						o << tab<<snamer.str() <<" <= "<< iSum.str()<<"("<<size<<" downto 0);"<<endl;
+						
+					if(i<pipe_levels) 
+						o << tab<< cOutR.str() <<" <= "<< iSum.str()<<"("<<size<<");"<<endl;
 				}
 			}
 			// Then the output to R 
 			for(int i=0; i<=pipe_levels; i++){
-				if (!((i==pipe_levels)&&	(last_chunk_size==0)))
+				if (!((i==pipe_levels)&&(last_chunk_size==0)))
 				{
 					int maxIndex, size;
 					if(i==pipe_levels) {
@@ -220,6 +255,8 @@ void IntAdder::output_vhdl(std::ostream& o, std::string name) {
 	o << "end architecture;" << endl << endl;
 }
 
+
+
 TestIOMap IntAdder::getTestIOMap()
 {
 	TestIOMap tim;
@@ -238,7 +275,9 @@ void IntAdder::fillTestCase(mpz_class a[])
 	mpz_class& svR = a[3];
 
 	svR = svX + svY + svC;
-	/* Don't allow overflow */
+	// Don't allow overflow
 	mpz_clrbit(svR.get_mpz_t(),wIn); 
+	
 }
+
 
