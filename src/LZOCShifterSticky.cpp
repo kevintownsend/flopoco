0f46549dd37c08381397156d57f4fbcbc8b98d87
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2009-02-12 17:00:22 +0000

    LZOCShifterSticky is being ported to framework 2.0. Still some work to do to have it passing the tests.

--- src/LZOCShifterSticky.cpp
+++ src/LZOCShifterSticky.cpp
@@ -37,174 +37,197 @@ using namespace std;
 LZOCShifterSticky::LZOCShifterSticky(Target* target, int wIn, int wOut, bool computeSticky, const int countType) :
 	Operator(target), wIn_(wIn), wOut_(wOut), computeSticky_(computeSticky), countType_(countType) {
 	
-	int i, p2i;
-
-	if (countType_ < 0)
-		setEntityType(gen);
-	else
-		setEntityType(spec);
+	ostringstream currLevel, prevLevel, currSticky, currCount;
+	
+	// -------- Parameter set up -----------------
+	setEntityType( (countType_==-1?gen:spec) );
+	wCount_ = intlog2(wIn_);
+	cout << " wCount is: "<<wCount_<<endl;
 
 	setOperatorName();
 	setOperatorType();
-	setPipelineDepth(0);//initialization
 
-	/* Set up the internal architecture signals */
-
-	/* Terminology: 
-	   - level i is the signal on which we will test wether the 2^(i-1) leading bits are zeroes.
-	   - i goes from wCount_ to 0 */
-	
-	/*  from the end of the pipeline to the beginning, 
-	    determine the sizes of all register levels */
-
-	if(wOut_<wIn_) {
-		i=0;
-		size_[0] = wOut_; /* size of the result */
-		while(size_[i]-wOut_ < wIn_){
-			i++;
-			p2i = 1<<(i-1);
-			size_[i] = size_[i-1] + p2i;
-			/* Invariant: size_[i] = wOut_ + 2^i -1 */
-		}
-		/* the attribute that gives the number of bits of the LZO count */
-		wCount_ = i;
-		/* the first stage doesn't need to register zeroes */
-		size_[wCount_] = 1<<wCount_;
-	
-		/* should be identical to : wCount_ = intlog2(wIn_+1); 
-			+1 for the case all zeroes */
-	}
-	else { // wOut>=wIn 	
-		wCount_ = intlog2(wIn_+1);
-		for(i=0; i<=wCount_; i++) {
-			size_[i] = wOut;
-		}
-	}
-
-	/* Set up the IO signals */
 	addInput ("I", wIn_);
-	/* The width of the lzo count output will be floor(log2(wIn_+1)). */		
+	if (entityType_==gen) addInput ("OZb"); /* if we generate a generic LZOC */
 	addOutput("Count", wCount_);
 	addOutput("O", wOut_);
-	/* if we require a sticky bit computation */
-	if(computeSticky_) 
-		addOutput("Sticky"); 
-	/* if we generate a generic LZOC */
-	if (entityType_==gen) 
-		addInput ("OZb"); 
-		
-	if(verbose){
-		cout <<endl<<"  wCount_=" << wCount_ << "    Level sizes: ";
-		for(i=0; i<=wCount_; i++) 
-			cout << size_[i]<<" ";
-		cout <<endl;
+	if (computeSticky_)   addOutput("Sticky"); /* if we require a sticky bit computation */
+	
+	currLevel << "level" <<wCount_;
+	currSticky <<"sticky"<<wCount_; 
+	
+	vhdl << tab << declare(currLevel.str(), wIn_) << " <= I ;"  <<endl; 
+	if (computeSticky_) vhdl << tab << declare(currLevel.str(), 1  ) << " <= '0' ;"<<endl; //init sticky 
+	
+	for (int i=wCount_-1; i>=0; i--){
+		currLevel.str(""); currLevel << "level" << i;
+		prevLevel.str(""); prevLevel << "level" << i+1;
+		currCount.str(""); currCount << "count" << i;
+	
+		vhdl << tab << declare(currCount.str(),1) << "<= '1' when " <<use(prevLevel.str())<<"("<< wIn_-1 <<" downto "<< wIn_ - intpow2(i) <<") = "
+		     << "("<< wIn-1 <<" downto "<< wIn - intpow2(i) <<"=>"<< (countType_==-1? use("sozb"): countType_==0?"'0'":"'1'")<<") else '0';"<<endl;
+		vhdl << tab << declare(currLevel.str(),wIn_) << "<= " << use(prevLevel.str()) << " when " << use(currCount.str()) << "='0' else "
+		     << use(prevLevel.str()) << "(" << wIn - intpow2(i)-1 << " downto 0) & (" << intpow2(i)-1 << " downto 0 => "
+		     << (countType_==-1? "not("+use("sozb")+")": countType_==0?"'1'":"'0'")<<");"<<endl;
+		      
 	}
-	//initialization needed for windows compiler
-	for (int i=0;i<42;i++)
-		countDepth_[i]=0;
-	double criticalPath = 0.0;
-	for (int i=wCount_; i>=0; i--){
-		ostringstream levelName, leveldName, stickyName;
-		levelName << "level"  << i;
-		stickyName << "sticky" << i;
-		level_[i] = levelName.str();
-
-	double stageDelay;
-	if (entityType_==gen) 
-		stageDelay =  2* target->localWireDelay() * (1<<i);
-	else		
-		stageDelay =  1.2 * target->localWireDelay() * (1<<i);		//TODO
-
-
-		if (i==wCount_)
-		{
-			criticalPath=stageDelay;
-			levelRegistered_[i] = false;
-		}	
-		else
-			if (criticalPath + stageDelay > 1/target->frequency()) {
-				criticalPath=stageDelay;
-				levelRegistered_[i] = true;
-				incrementPipelineDepth();
-			}
+	vhdl << tab << "O <= "<<use(currLevel.str())<<";"<<endl;
+	vhdl << tab << "Count <= ";
+	for (int i=wCount_-1; i>=0; i--){
+		currCount.str(""); currCount << "count" << i;
+		vhdl << use(currCount.str());
+		if (i>0) vhdl << " & ";
+		else     vhdl << ";"<<endl;
 	}
 	
-	if (isSequential())
-	{
-		for (int i=wCount_; i>=0; i--){
-			for (int j=0; j<=i; j++)
-				if (levelRegistered_[j])
-					countDepth_[i]++;	
-			if (levelRegistered_[i]){
-				leveld_[i] = level_[i] + "_d" ;
-				ostringstream levelName;
-				levelName << "level"<<i;		
-				addDelaySignal(levelName.str(), size_[i],1);	
-			}
-			else{
-				leveld_[i] = level_[i];
-				ostringstream levelName;
-				levelName << "level"<<i;		
-				addSignal(levelName.str(), size_[i]);	
-			}
-		}
 	
-		for (int i=wCount_-1; i>=0; i--){
-			ostringstream countName;
-			countName << "count"<<i;		
-			addDelaySignal(countName.str(), 1, countDepth_[i]);
-		}
-			
-		if(computeSticky_){
-			for (int j=wCount_; j>=0; j--){
-				ostringstream stickyName;
-				ostringstream eqVer;
-				ostringstream newSticky;
-				eqVer<<"eqVer"<<j;
-				newSticky<<"newSticky"<<j;
-				stickyName<<"sticky"<<j;
-				
-				if (levelRegistered_[j])			
-					addDelaySignal(stickyName.str(),1,1);
-				else
-					addSignal(stickyName.str());
-			
-				addSignal(eqVer.str());
-				addSignal(newSticky.str());	
-			}
-		}						
-			
-		if (entityType_==gen)
-			addDelaySignal("sozb",1, getPipelineDepth());
-	}else /* combinatorial version */
-	{
-		setPipelineDepth(0);
-		for (int i=wCount_; i>=0; i--){
-				leveld_[i] = level_[i];
-				ostringstream levelName;
-				levelName << "level"<<i;		
-				addSignal(levelName.str(), size_[i]);
-				if(computeSticky_){
-					ostringstream stickyName;
-					ostringstream eqVer;
-					ostringstream newSticky;
-					stickyName<<"sticky"<<i;
-					eqVer<<"eqVer"<<i;
-					newSticky<<"newSticky"<<i;
-					addSignal(stickyName.str());
-					addSignal(eqVer.str());
-					addSignal(newSticky.str());
-				}					
-		}
-		
-		for (int i=wCount_-1; i>=0; i--){
-			ostringstream countName;
-			countName << "count"<<i;		
-			addSignal(countName.str(), 1);
-		}
-		addSignal("sozb",1);
-	}
-	addSignal("preCount", wCount_);	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	
+//	
+//	
+//	
+//		
+//	if(verbose){
+//		cout <<endl<<"  wCount_=" << wCount_ << "    Level sizes: ";
+//		for(i=0; i<=wCount_; i++) 
+//			cout << size_[i]<<" ";
+//		cout <<endl;
+//	}
+//	//initialization needed for windows compiler
+//	for (int i=0;i<42;i++)
+//		countDepth_[i]=0;
+//	double criticalPath = 0.0;
+//	for (int i=wCount_; i>=0; i--){
+//		ostringstream levelName, leveldName, stickyName;
+//		levelName << "level"  << i;
+//		stickyName << "sticky" << i;
+//		level_[i] = levelName.str();
+
+//	double stageDelay;
+//	if (entityType_==gen) 
+//		stageDelay =  2* target->localWireDelay() * (1<<i);
+//	else		
+//		stageDelay =  1.2 * target->localWireDelay() * (1<<i);		//TODO
+
+
+//		if (i==wCount_)
+//		{
+//			criticalPath=stageDelay;
+//			levelRegistered_[i] = false;
+//		}	
+//		else
+//			if (criticalPath + stageDelay > 1/target->frequency()) {
+//				criticalPath=stageDelay;
+//				levelRegistered_[i] = true;
+//				incrementPipelineDepth();
+//			}
+//	}
+//	
+//	if (isSequential())
+//	{
+//		for (int i=wCount_; i>=0; i--){
+//			for (int j=0; j<=i; j++)
+//				if (levelRegistered_[j])
+//					countDepth_[i]++;	
+//			if (levelRegistered_[i]){
+//				leveld_[i] = level_[i] + "_d" ;
+//				ostringstream levelName;
+//				levelName << "level"<<i;		
+//				addDelaySignal(levelName.str(), size_[i],1);	
+//			}
+//			else{
+//				leveld_[i] = level_[i];
+//				ostringstream levelName;
+//				levelName << "level"<<i;		
+//				addSignal(levelName.str(), size_[i]);	
+//			}
+//		}
+//	
+//		for (int i=wCount_-1; i>=0; i--){
+//			ostringstream countName;
+//			countName << "count"<<i;		
+//			addDelaySignal(countName.str(), 1, countDepth_[i]);
+//		}
+//			
+//		if(computeSticky_){
+//			for (int j=wCount_; j>=0; j--){
+//				ostringstream stickyName;
+//				ostringstream eqVer;
+//				ostringstream newSticky;
+//				eqVer<<"eqVer"<<j;
+//				newSticky<<"newSticky"<<j;
+//				stickyName<<"sticky"<<j;
+//				
+//				if (levelRegistered_[j])			
+//					addDelaySignal(stickyName.str(),1,1);
+//				else
+//					addSignal(stickyName.str());
+//			
+//				addSignal(eqVer.str());
+//				addSignal(newSticky.str());	
+//			}
+//		}						
+//			
+//		if (entityType_==gen)
+//			addDelaySignal("sozb",1, getPipelineDepth());
+//	}else /* combinatorial version */
+//	{
+//		setPipelineDepth(0);
+//		for (int i=wCount_; i>=0; i--){
+//				leveld_[i] = level_[i];
+//				ostringstream levelName;
+//				levelName << "level"<<i;		
+//				addSignal(levelName.str(), size_[i]);
+//				if(computeSticky_){
+//					ostringstream stickyName;
+//					ostringstream eqVer;
+//					ostringstream newSticky;
+//					stickyName<<"sticky"<<i;
+//					eqVer<<"eqVer"<<i;
+//					newSticky<<"newSticky"<<i;
+//					addSignal(stickyName.str());
+//					addSignal(eqVer.str());
+//					addSignal(newSticky.str());
+//				}					
+//		}
+//		
+//		for (int i=wCount_-1; i>=0; i--){
+//			ostringstream countName;
+//			countName << "count"<<i;		
+//			addSignal(countName.str(), 1);
+//		}
+//		addSignal("sozb",1);
+//	}
+//	addSignal("preCount", wCount_);	
 }
 
 LZOCShifterSticky::~LZOCShifterSticky() {
@@ -238,131 +261,135 @@ void LZOCShifterSticky::setOperatorName(){
 void LZOCShifterSticky::outputVHDL(std::ostream& o, std::string name) {
 	licence(o,"Florent de Dinechin, Bogdan Pasca (2007)");
 	Operator::stdLibs(o);
+	Operator::stdLibs(o);
 	outputVHDLEntity(o);
 	newArchitecture(o,name);
-	outputVHDLSignalDeclarations(o);	
+	o << buildVHDLSignalDeclarations();
 	beginArchitecture(o);
-	
-	/* connect input to level wCount_, possibly with 0 padding */
-	o << tab << "level" << wCount_<< " <= " ;
-	if(wIn_ == size_[wCount_]) // no padding needed
-		o << "I;" <<endl;
-	else 
-		o << "I & (" << size_[wCount_]-wIn_-1 << " downto 0 => '0');" << endl ;
-
-	if (entityType_==gen)
-		o<<tab<<"sozb <= OZb;"<<endl;
-	
-	if(computeSticky_)
-		o<<tab<<"sticky" << wCount_ << " <= '0';"<<endl;
-
-	for  (int i = wCount_; i>=1; i--){
-		//int p2i = 1 << i;
-		int p2io2 = 1 << (i-1);
-
-		//=====================COUNT=================================================
-		o << tab << "count" << i-1 << " <= '1' "
-		  << "when " << leveld_[i] << "(" << size_[i]-1 << " downto " << size_[i]-p2io2 << ") "
-		  << "= (" << size_[i]-1 << " downto " << size_[i]-p2io2 << " => "; 
-		if (entityType_==gen)
-			o<<delaySignal("sozb",getPipelineDepth()-countDepth_[i-1]);
-		else
-			o<<"'"<<countType_<<"'";
-		o<<" )   else '0';" << endl; 
-
-		//There must be a unified way of unifying the two branches, but it's clearer this way
-		if(wOut_<wIn_) {
-			//=====================SHIFT=================================================
-			// REM in the following,  size_[i]-size_[i-1]-1 is almost always equal to p2io2, except in the first stage
-			if (i==wCount_){ //first stage
-				o << tab << level_[i-1] << " <= " ;
-				o << " " << "("<<leveld_[i] << "(" << size_[i]/2 -1 << " downto 0)";
-				if ((size_[i-1]-size_[i]/2 -1)>=0)
-					o << "& (" << size_[i-1]-size_[i]/2 -1 << " downto 0 => '0')";
-				o << " )  when count" << i-1 << "='1'" << endl;
-				
-				if (size_[i-1]-size_[i]>0)
-					o << tab << tab << tab <<  "else (" << leveld_[i] << "(" << size_[i]-1 << " downto 0) &  "<<zeroGenerator(size_[i-1]-size_[i],0)<<") ;" << endl; 
-				else
-					o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i]-1 << " downto "<<size_[i]-size_[i-1]<<");" << endl; 
-			}
-			else {  // generic stage
-				o << tab << level_[i-1] << " <= " ;
-				if (size_[i-1]-1 > 0){
-					o << " " << leveld_[i] << "(" << size_[i]-1 << " downto " << size_[i]-size_[i-1] << ")   when count" << i-1 << "='0'" << endl;
-					o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i-1]-1 << " downto 0);" << endl; 
-				}else{
-					o << " " << leveld_[i] << "(" << size_[i]-1<<")   when count" << i-1 << "='0'" << endl;
-					o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i-1]-1 << ");" << endl; 
-				}
-			}
-		}
-		else { // wOut=wIn 	
-			o << tab << level_[i-1] << " <= " ;
-			o << " " << leveld_[i] << " when count" << i-1 << "='0'" << endl;
-			o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i-1] - p2io2 -1 << " downto 0) "
-			  << " & (" << p2io2-1 << " downto 0 => '0');" << endl; 
-		
-		}
-		if(computeSticky_){
-			ostringstream eqVer;
-			ostringstream newSticky;
-			eqVer << "eqVer"<<i;
-			newSticky <<"newSticky"<<i;
-			
-			o << tab << eqVer.str() <<" <= '1' when " << leveld_[i] << "(" << size_[i]-size_[i-1]-1 << " downto 0) = (" << size_[i]-size_[i-1]-1 << " downto 0 => '0') else '0';"
-			  << endl;
-			o << tab << newSticky.str() <<" <= sticky" << i << " or not("<<eqVer.str()<<");"<<endl;  
-		
-			if (size_[i]-size_[i-1]-1>=0){
-				o << tab << "sticky" << i-1 << " <= sticky"<<i;
-				if(isSequential() && i<wCount_ && levelRegistered_[i]) 
-					o << "_d";
-				o <<" when (count" << i-1 << "='1') else "<<newSticky.str();
-			}
-			else{
-				o << tab << "sticky" << i-1 << " <= sticky"<<i<<" when count" << i-1 << "='1'  else sticky" << i;	
-				if(isSequential() && i<wCount_ && levelRegistered_[i]) 
-					o << "_d";
-			}
-			o << ";" << endl;
-		}
-			
-		o << endl;
-	}
-
-	if(computeSticky_){
-		if (levelRegistered_[0]){
-			o << tab << "sticky <= sticky0_d;" << endl;
-		}else{
-			o << tab << "sticky <= sticky0;" << endl;
-		}
-	}
-	o << tab << "O      <= "<<leveld_[0]<<";" << endl;
-	o << tab << "preCount  <= ";
-	for (int i=wCount_-1; i >=0; i--){
-		ostringstream name;
-		name << "count" << i;
-		if (isSequential())
-			o << delaySignal(name.str(), countDepth_[i]);
-		else
-			o << name.str();
-		if (i>0) o << " & ";
-	}
-	
-	o << ";" << endl;
-
-	ostringstream binInputWidth;
-	
-	printBinNum(binInputWidth, wIn_, wCount_);	
-
-	o << tab << "Count <= \""<<binInputWidth.str()<<"\" when preCount = ("<<wCount_-1<<" downto  0 => '1')";
-	o << tab << "         else preCount;"<<endl;
-	 
-	if (isSequential()){		
-		outputVHDLRegisters(o); o<<endl;
-	}
+	o << buildVHDLRegisters();
+	o << vhdl.str();
 	endArchitecture(o);
+//	
+//	/* connect input to level wCount_, possibly with 0 padding */
+//	o << tab << "level" << wCount_<< " <= " ;
+//	if(wIn_ == size_[wCount_]) // no padding needed
+//		o << "I;" <<endl;
+//	else 
+//		o << "I & (" << size_[wCount_]-wIn_-1 << " downto 0 => '0');" << endl ;
+
+//	if (entityType_==gen)
+//		o<<tab<<"sozb <= OZb;"<<endl;
+//	
+//	if(computeSticky_)
+//		o<<tab<<"sticky" << wCount_ << " <= '0';"<<endl;
+
+//	for  (int i = wCount_; i>=1; i--){
+//		//int p2i = 1 << i;
+//		int p2io2 = 1 << (i-1);
+
+//		//=====================COUNT=================================================
+//		o << tab << "count" << i-1 << " <= '1' "
+//		  << "when " << leveld_[i] << "(" << size_[i]-1 << " downto " << size_[i]-p2io2 << ") "
+//		  << "= (" << size_[i]-1 << " downto " << size_[i]-p2io2 << " => "; 
+//		if (entityType_==gen)
+//			o<<delaySignal("sozb",getPipelineDepth()-countDepth_[i-1]);
+//		else
+//			o<<"'"<<countType_<<"'";
+//		o<<" )   else '0';" << endl; 
+
+//		//There must be a unified way of unifying the two branches, but it's clearer this way
+//		if(wOut_<wIn_) {
+//			//=====================SHIFT=================================================
+//			// REM in the following,  size_[i]-size_[i-1]-1 is almost always equal to p2io2, except in the first stage
+//			if (i==wCount_){ //first stage
+//				o << tab << level_[i-1] << " <= " ;
+//				o << " " << "("<<leveld_[i] << "(" << size_[i]/2 -1 << " downto 0)";
+//				if ((size_[i-1]-size_[i]/2 -1)>=0)
+//					o << "& (" << size_[i-1]-size_[i]/2 -1 << " downto 0 => '0')";
+//				o << " )  when count" << i-1 << "='1'" << endl;
+//				
+//				if (size_[i-1]-size_[i]>0)
+//					o << tab << tab << tab <<  "else (" << leveld_[i] << "(" << size_[i]-1 << " downto 0) &  "<<zeroGenerator(size_[i-1]-size_[i],0)<<") ;" << endl; 
+//				else
+//					o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i]-1 << " downto "<<size_[i]-size_[i-1]<<");" << endl; 
+//			}
+//			else {  // generic stage
+//				o << tab << level_[i-1] << " <= " ;
+//				if (size_[i-1]-1 > 0){
+//					o << " " << leveld_[i] << "(" << size_[i]-1 << " downto " << size_[i]-size_[i-1] << ")   when count" << i-1 << "='0'" << endl;
+//					o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i-1]-1 << " downto 0);" << endl; 
+//				}else{
+//					o << " " << leveld_[i] << "(" << size_[i]-1<<")   when count" << i-1 << "='0'" << endl;
+//					o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i-1]-1 << ");" << endl; 
+//				}
+//			}
+//		}
+//		else { // wOut=wIn 	
+//			o << tab << level_[i-1] << " <= " ;
+//			o << " " << leveld_[i] << " when count" << i-1 << "='0'" << endl;
+//			o << tab << tab << tab <<  "else " << leveld_[i] << "(" << size_[i-1] - p2io2 -1 << " downto 0) "
+//			  << " & (" << p2io2-1 << " downto 0 => '0');" << endl; 
+//		
+//		}
+//		if(computeSticky_){
+//			ostringstream eqVer;
+//			ostringstream newSticky;
+//			eqVer << "eqVer"<<i;
+//			newSticky <<"newSticky"<<i;
+//			
+//			o << tab << eqVer.str() <<" <= '1' when " << leveld_[i] << "(" << size_[i]-size_[i-1]-1 << " downto 0) = (" << size_[i]-size_[i-1]-1 << " downto 0 => '0') else '0';"
+//			  << endl;
+//			o << tab << newSticky.str() <<" <= sticky" << i << " or not("<<eqVer.str()<<");"<<endl;  
+//		
+//			if (size_[i]-size_[i-1]-1>=0){
+//				o << tab << "sticky" << i-1 << " <= sticky"<<i;
+//				if(isSequential() && i<wCount_ && levelRegistered_[i]) 
+//					o << "_d";
+//				o <<" when (count" << i-1 << "='1') else "<<newSticky.str();
+//			}
+//			else{
+//				o << tab << "sticky" << i-1 << " <= sticky"<<i<<" when count" << i-1 << "='1'  else sticky" << i;	
+//				if(isSequential() && i<wCount_ && levelRegistered_[i]) 
+//					o << "_d";
+//			}
+//			o << ";" << endl;
+//		}
+//			
+//		o << endl;
+//	}
+
+//	if(computeSticky_){
+//		if (levelRegistered_[0]){
+//			o << tab << "sticky <= sticky0_d;" << endl;
+//		}else{
+//			o << tab << "sticky <= sticky0;" << endl;
+//		}
+//	}
+//	o << tab << "O      <= "<<leveld_[0]<<";" << endl;
+//	o << tab << "preCount  <= ";
+//	for (int i=wCount_-1; i >=0; i--){
+//		ostringstream name;
+//		name << "count" << i;
+//		if (isSequential())
+//			o << delaySignal(name.str(), countDepth_[i]);
+//		else
+//			o << name.str();
+//		if (i>0) o << " & ";
+//	}
+//	
+//	o << ";" << endl;
+
+//	ostringstream binInputWidth;
+//	
+//	printBinNum(binInputWidth, wIn_, wCount_);	
+
+//	o << tab << "Count <= \""<<binInputWidth.str()<<"\" when preCount = ("<<wCount_-1<<" downto  0 => '1')";
+//	o << tab << "         else preCount;"<<endl;
+//	 
+//	if (isSequential()){		
+//		outputVHDLRegisters(o); o<<endl;
+//	}
+
 }
 
 
