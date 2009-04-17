b46aed05a786fb9db51a0a368c18bfd8cc7c0082
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2009-04-17 14:47:45 +0000

    added support for N=1. This is usefull for generaliations.

--- src/IntNAdder.cpp
+++ src/IntNAdder.cpp
@@ -193,7 +193,77 @@ Operator(target), wIn_(wIn), N_(N), inputDelays_(inputDelays)
 				k++;
 			}
 			vhdl << ";" <<endl;
-		}
+		}else
+			if (N==1){
+			//split the inputs ( this should be reusable )
+				for (int i=0;i<N;i++)
+					for (int j=0; j<nbOfChunks; j++){
+						ostringstream name;
+						//the naming standard: sX j _ i _ l
+						//j=the chunk index i is the input index and l is the current level
+						name << "sX"<<j<<"_"<<i<<"_l"<<0;
+						int low=0, high=0;
+						for (int k=0;k<=j;k++)
+							high+=cSize[k];
+						for (int k=0;k<=j-1;k++)
+							low+=cSize[k];
+						vhdl << tab << declare (name.str(),cSize[j]+1) << " <= " << " \"0\" & X"<<i<<range(high-1,low)<<";"<<endl;
+					}
+	
+				////////////////////////////////////////////////
+				int currentLevel = 1;
+//				for (int l=1; l<N; l++){
+//					//perform addition round; there is only one additon per round 
+//					for (int j=0; j<nbOfChunks; j++){
+//						ostringstream dname, uname1, uname2, uname3;
+//						dname << "sX"<<j<<"_0_l"<<l;
+//						uname1 << "sX"<<j<<"_0_l"<<l-1;
+//						uname2 << "sX"<<j<<"_1_l"<<l-1;
+//						uname3 << "sX"<<j-1<<"_0_l"<<l-1;
+//						vhdl << tab << declare(dname.str(),cSize[j]+1) << " <= (\"0\" & "<< use(uname1.str())<<range(cSize[j]-1,0) <<") +  (\"0\" & "<< use(uname2.str())<<range(cSize[j]-1,0)<<")";
+//						if ((j==0)&&(l==1)) vhdl << " + Cin";
+//						if (j>0) vhdl << " + " << use(uname3.str())<<"("<<cSize[j-1]<<") ";
+//						vhdl << ";" << endl;				}
+//		
+//					//from this point we just add two numbers with internal propagations, so perform addition and then take care of the propagations in a loop-like manner
+//					for (int propL=2; propL<=N-l; propL++)
+//						for (int j=0; j<nbOfChunks; j++){
+//							ostringstream dname, uname;
+//							dname << "sX"<<j<<"_"<<propL-1<<"_l"<<l;
+//							uname << "sX"<<j<<"_"<<propL<<"_l"<<l-1;
+//							vhdl << tab << declare(dname.str(), cSize[j]+1) << " <= " << use(uname.str()) << ";" <<endl;
+//						}
+//						nextCycle();
+//				currentLevel++;
+//				}
+				////////////////////////////////////////////////
+	
+				for (int i=2; i<nbOfChunks+2; i++){
+					for (int j=i-2; j< nbOfChunks ; j++){
+						ostringstream dname, uname1, uname2;
+						dname << "sX"<<j<<"_0_l"<<currentLevel;
+						uname1 << "sX"<<j<<"_0_l"<<currentLevel-1;
+						uname2 << "sX"<<j-1<<"_0_l"<<currentLevel-1;
+						if (j>0)
+							vhdl << tab << declare(dname.str(), cSize[j]+1) << " <= ( \"0\" & " << use(uname1.str())<<range(cSize[j]-1,0) << ") + " << use(uname2.str()) << "(" << cSize[j-1] << ")" << ";" <<endl;
+						else
+							vhdl << tab << declare(dname.str(), cSize[j]+1) << " <= ( \"0\" & " << use(uname1.str())<<range(cSize[j]-1,0) << ") + Cin ;" <<endl;
+					}
+				currentLevel++;
+				if (i<nbOfChunks+1) nextCycle();
+				}
+				currentLevel--;
+				vhdl << tab << "R <= ";
+				int k=0;
+				for (int i=nbOfChunks-1; i>=0; i--){
+					ostringstream uname;
+					uname << "sX"<<i<<"_0_l"<<currentLevel-k;
+					vhdl << use(uname.str()) << range(cSize[i]-1,0);
+					if (i > 0) vhdl << " & ";
+					k++;
+				}
+				vhdl << ";" <<endl;
+			}
 	}
 }
 
