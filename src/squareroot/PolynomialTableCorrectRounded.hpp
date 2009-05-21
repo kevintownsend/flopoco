2366972b06885d781e4c343041d4245e132a1d53
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2009-05-21 11:12:54 +0000

    added the polynomial tables for Correctly rounded version

---
+++ src/squareroot/PolynomialTableCorrectRounded.hpp
@@ -0,0 +1,34 @@
+#ifndef PolynomialTableCorrectRounded_H
+#define PolynomialTableCorrectRounded_H
+
+#include "../Table.hpp"
+
+class PolynomialTableCorrectRounded : public Table
+{
+public:
+	PolynomialTableCorrectRounded(Target* target, int p1, int w1);
+	
+	~PolynomialTableCorrectRounded();
+  	
+	mpz_class function(int x);
+	
+	//  int check_accuracy(int wF);
+	
+	int    double2input(double x);
+	
+	double input2double(int x);
+	
+	mpz_class double2output(double x);
+	
+	double output2double(mpz_class x);
+	
+	double maxMulOut;
+	double minMulOut;
+	
+private:
+	
+	
+};
+
+#endif
+
