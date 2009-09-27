c820e4660b0a682e021e9e4cd52821d6d8ce75ac
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2009-09-27 21:33:10 +0000

    more bla

--- src/Signal.cpp
+++ src/Signal.cpp
@@ -4,237 +4,241 @@
 
 using namespace std;
 
-Signal::Signal(const string name, const Signal::SignalType type, const int width, const bool isBus) : 
-	name_(name), type_(type), width_(width), numberOfPossibleValues_(1), lifeSpan_(0),  cycle_(0),	
-	isFP_(false), wE_(0), wF_(0), isSubSignal_(false), low_(0), high_(width-1), isBus_(isBus) {
-	updateSignalName();
-}
-
-Signal::Signal(const string name, const Signal::SignalType type, const int wE, const int wF) : 
-	name_(name), type_(type), width_(wE+wF+3), numberOfPossibleValues_(1), lifeSpan_(0), cycle_(0),
-	isFP_(true), wE_(wE), wF_(wF), isSubSignal_(false),
-	low_(0), high_(width_-1), isBus_(false)
-{
-	updateSignalName();
-}
-
-Signal::~Signal(){}
-
-const string& Signal::getName() const { 
-	return id_; 
-}
-
-void Signal::updateSignalName()	{
-	if (isSubSignal_ == false)
-		id_ = name_;
-	else
-		{
-			stringstream o;
-			if (width_ == 1)
-				o << name_ << "(" << low_ << ")";
-			else
-				o << name_ << "(" << high_ << " downto " << low_ << ")";
-			id_ = o.str();
-		}
-}
-
-int Signal::width() const{return width_;}
-	
-int Signal::wE() const {return(wE_);}
-
-int Signal::wF() const {return(wF_);}
-	
-bool Signal::isFP() const {return isFP_;}
-
-bool Signal::isBus() const {return isBus_;}
-
-Signal::SignalType Signal::type() const {return type_;}
-	
-string Signal::toVHDL() {
-	ostringstream o; 
-	if(type()==Signal::wire || type()==Signal::registeredWithoutReset || type()==Signal::registeredWithAsyncReset || type()==Signal::registeredWithSyncReset) 
-		o << "signal ";
-	o << getName();
-	o << " : ";
-	if (type()==Signal::in)
-		o << "in ";
-	if(type()==Signal::out)
-		o << "out ";
-	
-	if ((1==width())&&(!isBus_)) 
-		o << "std_logic" ;
-	else 
-		if(isFP_) 
-			o << " std_logic_vector(" << wE() <<"+"<<wF() << "+2 downto 0)";
-		else
-			o << " std_logic_vector(" << width()-1 << " downto 0)";
-	return o.str();
-}
+namespace flopoco{
 
 
-
-string Signal::delayedName(int delay){
-	ostringstream o;
-#if 0
-	o << getName();
-	if(delay>0) {
-		for (int i=0; i<delay; i++){
-			o  << "_d";
-		}
+	Signal::Signal(const string name, const Signal::SignalType type, const int width, const bool isBus) : 
+		name_(name), type_(type), width_(width), numberOfPossibleValues_(1), lifeSpan_(0),  cycle_(0),	
+		isFP_(false), wE_(0), wF_(0), isSubSignal_(false), low_(0), high_(width-1), isBus_(isBus) {
+		updateSignalName();
 	}
+
+	Signal::Signal(const string name, const Signal::SignalType type, const int wE, const int wF) : 
+		name_(name), type_(type), width_(wE+wF+3), numberOfPossibleValues_(1), lifeSpan_(0), cycle_(0),
+		isFP_(true), wE_(wE), wF_(wF), isSubSignal_(false),
+		low_(0), high_(width_-1), isBus_(false)
+	{
+		updateSignalName();
+	}
+
+	Signal::~Signal(){}
+
+	const string& Signal::getName() const { 
+		return id_; 
+	}
+
+	void Signal::updateSignalName()	{
+		if (isSubSignal_ == false)
+			id_ = name_;
+		else
+			{
+				stringstream o;
+				if (width_ == 1)
+					o << name_ << "(" << low_ << ")";
+				else
+					o << name_ << "(" << high_ << " downto " << low_ << ")";
+				id_ = o.str();
+			}
+	}
+
+	int Signal::width() const{return width_;}
+	
+	int Signal::wE() const {return(wE_);}
+
+	int Signal::wF() const {return(wF_);}
+	
+	bool Signal::isFP() const {return isFP_;}
+
+	bool Signal::isBus() const {return isBus_;}
+
+	Signal::SignalType Signal::type() const {return type_;}
+	
+	string Signal::toVHDL() {
+		ostringstream o; 
+		if(type()==Signal::wire || type()==Signal::registeredWithoutReset || type()==Signal::registeredWithAsyncReset || type()==Signal::registeredWithSyncReset) 
+			o << "signal ";
+		o << getName();
+		o << " : ";
+		if (type()==Signal::in)
+			o << "in ";
+		if(type()==Signal::out)
+			o << "out ";
+	
+		if ((1==width())&&(!isBus_)) 
+			o << "std_logic" ;
+		else 
+			if(isFP_) 
+				o << " std_logic_vector(" << wE() <<"+"<<wF() << "+2 downto 0)";
+			else
+				o << " std_logic_vector(" << width()-1 << " downto 0)";
+		return o.str();
+	}
+
+
+
+	string Signal::delayedName(int delay){
+		ostringstream o;
+#if 0
+		o << getName();
+		if(delay>0) {
+			for (int i=0; i<delay; i++){
+				o  << "_d";
+			}
+		}
 #else // someday we need to civilize pipe signal names
-	o << getName();
-	if(delay>0) 
-		o << "_d" << delay;
+		o << getName();
+		if(delay>0) 
+			o << "_d" << delay;
 #endif
 		return o.str();
-}
-
-
-string Signal::toVHDLDeclaration() {
-	ostringstream o; 
-	o << "signal ";
-	if (type_!=Signal::in)
-		o << getName() << (lifeSpan_ > 0 ? ", ": "");
-	if (lifeSpan_ > 0)
-		o << getName() << "_d" << 1;
-	for (int i=2; i<=lifeSpan_; i++) {
-			o << ", " << getName() << "_d" << i;
 	}
-	o << " : ";
-	if ((1==width())&&(!isBus_)) 
-		o << "std_logic" ;
-	else 
-		if(isFP_) 
-			o << " std_logic_vector(" << wE() <<"+"<<wF() << "+2 downto 0)";
-		else
-			o << " std_logic_vector(" << width()-1 << " downto 0)";
-	o << ";";
-	return o.str();
-}
 
-Signal Signal::getSubSignal(int low, int high)
-{
-	if (low < low_)
-		throw string("Attempted to return subsignal with smaller low index.");
-	if (high > high_)
-		throw string("Attempted to return subsignal with bigger high index."); 
+
+	string Signal::toVHDLDeclaration() {
+		ostringstream o; 
+		o << "signal ";
+		if (type_!=Signal::in)
+			o << getName() << (lifeSpan_ > 0 ? ", ": "");
+		if (lifeSpan_ > 0)
+			o << getName() << "_d" << 1;
+		for (int i=2; i<=lifeSpan_; i++) {
+			o << ", " << getName() << "_d" << i;
+		}
+		o << " : ";
+		if ((1==width())&&(!isBus_)) 
+			o << "std_logic" ;
+		else 
+			if(isFP_) 
+				o << " std_logic_vector(" << wE() <<"+"<<wF() << "+2 downto 0)";
+			else
+				o << " std_logic_vector(" << width()-1 << " downto 0)";
+		o << ";";
+		return o.str();
+	}
+
+	Signal Signal::getSubSignal(int low, int high)
+	{
+		if (low < low_)
+			throw string("Attempted to return subsignal with smaller low index.");
+		if (high > high_)
+			throw string("Attempted to return subsignal with bigger high index."); 
 	
-	Signal s(name_, type_, high-low+1);
-	s.low_ = low;
-	s.high_ = high;
-	s.isSubSignal_ = true;
+		Signal s(name_, type_, high-low+1);
+		s.low_ = low;
+		s.high_ = high;
+		s.isSubSignal_ = true;
 	
 		return s;
-}
+	}
 	
-Signal Signal::getException()
-{
-	if (!isFP_)
-		throw string("Not a floating point signal.");
-	return getSubSignal(1+wE_+wF_, 2+wE_+wF_);
-}
-
-Signal Signal::getSign()
-{
-	if (!isFP_)
+	Signal Signal::getException()
+	{
+		if (!isFP_)
 			throw string("Not a floating point signal.");
-	return getSubSignal(wE_+wF_, wE_+wF_);
-}
-
-Signal Signal::getExponent()
-{
-	if (!isFP_)
-		throw string("Not a floating point signal.");
-	return getSubSignal(wF_, wE_+wF_-1);
-}
-
-Signal Signal::getMantissa()
-{
-	if (!isFP_)
-		throw string("Not a floating point signal.");
-	return getSubSignal(0, wF_-1);
-}
-
-
-void Signal::setCycle(int cycle) {
-	cycle_ = cycle;
-}
-
-int Signal::getCycle() {
-	return cycle_;
-}
-
-void Signal::updateLifeSpan(int delay) {
-	if(delay>lifeSpan_)
-		lifeSpan_=delay;
-}
-
-int Signal::getLifeSpan() {
-	return lifeSpan_;
-}
-
-
-void  Signal::setNumberOfPossibleValues(int n){
-	numberOfPossibleValues_ = n;
-}
-
-
-int  Signal::getNumberOfPossibleValues(){
-	return numberOfPossibleValues_;
-}
-
-
-
-std::string Signal::valueToVHDL(mpz_class v, bool quot){
-	std::string r;
-
-	/* Get base 2 representation */
-	r = v.get_str(2);
-
-	/* Some checks */
-	if ((int) r.size() > width())	{
-		std::ostringstream o;
-		o << "Error in " <<  __FILE__ << "@" << __LINE__ << ": value (" << r << ") is larger than signal " << getName();
-		throw o.str();
+		return getSubSignal(1+wE_+wF_, 2+wE_+wF_);
 	}
 
-	/* Do padding */
-	while ((int)r.size() < width())
-		r = "0" + r;
-
-	/* Put apostrophe / quot */
-	if (!quot) return r;
-	if (width() > 1)
-		return "\"" + r + "\"";
-	else
-		return "'" + r + "'";
-}
-
-
-std::string Signal::valueToVHDLHex(mpz_class v, bool quot){
-	std::string o;
-
-	/* Get base 16 representation */
-	o = v.get_str(16);
-
-	/* Some check */
-	/* XXX: Too permissive */
-	if ((int)o.size() * 4 > width() + 4)	{
-		std::ostringstream o;
-		o << "Error in " <<  __FILE__ << "@" << __LINE__ << ": value is larger than signal " << getName();
-		throw o.str();
+	Signal Signal::getSign()
+	{
+		if (!isFP_)
+			throw string("Not a floating point signal.");
+		return getSubSignal(wE_+wF_, wE_+wF_);
 	}
 
-	/* Do padding */
-	while ((int)o.size() * 4 < width())
-		o = "0" + o;
+	Signal Signal::getExponent()
+	{
+		if (!isFP_)
+			throw string("Not a floating point signal.");
+		return getSubSignal(wF_, wE_+wF_-1);
+	}
 
-	/* Put apostrophe / quot */
-	if (!quot) return o;
-	if (width() > 1)
-		return "x\"" + o + "\"";
-	else
-		return "'" + o + "'";
+	Signal Signal::getMantissa()
+	{
+		if (!isFP_)
+			throw string("Not a floating point signal.");
+		return getSubSignal(0, wF_-1);
+	}
+
+
+	void Signal::setCycle(int cycle) {
+		cycle_ = cycle;
+	}
+
+	int Signal::getCycle() {
+		return cycle_;
+	}
+
+	void Signal::updateLifeSpan(int delay) {
+		if(delay>lifeSpan_)
+			lifeSpan_=delay;
+	}
+
+	int Signal::getLifeSpan() {
+		return lifeSpan_;
+	}
+
+
+	void  Signal::setNumberOfPossibleValues(int n){
+		numberOfPossibleValues_ = n;
+	}
+
+
+	int  Signal::getNumberOfPossibleValues(){
+		return numberOfPossibleValues_;
+	}
+
+
+
+	std::string Signal::valueToVHDL(mpz_class v, bool quot){
+		std::string r;
+
+		/* Get base 2 representation */
+		r = v.get_str(2);
+
+		/* Some checks */
+		if ((int) r.size() > width())	{
+			std::ostringstream o;
+			o << "Error in " <<  __FILE__ << "@" << __LINE__ << ": value (" << r << ") is larger than signal " << getName();
+			throw o.str();
+		}
+
+		/* Do padding */
+		while ((int)r.size() < width())
+			r = "0" + r;
+
+		/* Put apostrophe / quot */
+		if (!quot) return r;
+		if (width() > 1)
+			return "\"" + r + "\"";
+		else
+			return "'" + r + "'";
+	}
+
+
+	std::string Signal::valueToVHDLHex(mpz_class v, bool quot){
+		std::string o;
+
+		/* Get base 16 representation */
+		o = v.get_str(16);
+
+		/* Some check */
+		/* XXX: Too permissive */
+		if ((int)o.size() * 4 > width() + 4)	{
+			std::ostringstream o;
+			o << "Error in " <<  __FILE__ << "@" << __LINE__ << ": value is larger than signal " << getName();
+			throw o.str();
+		}
+
+		/* Do padding */
+		while ((int)o.size() * 4 < width())
+			o = "0" + o;
+
+		/* Put apostrophe / quot */
+		if (!quot) return o;
+		if (width() > 1)
+			return "x\"" + o + "\"";
+		else
+			return "'" + o + "'";
+	}
+
 }
-
--- src/Signal.hpp
+++ src/Signal.hpp
@@ -11,201 +11,204 @@
   #include <inttypes.h>
 #endif
 
-
-/**
- * A class representing a signal. This is the basic block that operators use
- */
-class Signal
-{
-public:
-	/** The possible types of a signal*/
-	typedef enum {
-	in,                        /**< if the signal is an input signal */
-	out,                       /**< if the signal is an output signal */
-	wire,                      /**< if the signal is a wire (not registered) */
-	registeredWithoutReset,    /**< if the signal is registered, but does not have a reset */
-	registeredWithAsyncReset,  /**< if the signal is registered, and has an asynchronous reset */
-	registeredWithSyncReset    /**< if the signal is registered, and has an synchronous reset */
-	} SignalType;
-
-	/** Signal constructor.
-	 * The standard constructor for signals which are not floating-point.
-	 * @param name      the name of the signal
-	 * @param type      the type of the signal, @see SignalType
-	 * @param width     the width of the signal
-	 * @param isBus     the flag which signals if the signal is a bus (std_logic_vector)
-	 */
-	Signal(const std::string name, const Signal::SignalType type, const int width = 1, const bool isBus=false);
-	/** Signal constructor.
-	 * The standard constructor for signals which are floating-point.
-	 * @param name      the name of the signal
-	 * @param type      the type of the signal, @see SignalType
-	 * @param wE        the exponent width
-	 * @param wF        the significand width
-	 */
-	Signal(const std::string name, const SignalType type, const int wE, const int wF);
-
-	/** Signal destructor.
-	 */		
-	~Signal();
-	
-	/** Returns the name of the signal
-	 * @return the name of the signal
-	 */	
-	const std::string& getName() const;
-
-	/** Updates the name of the signal.
-	 * It takes into consideration the fact that we might have subsignals
-	 */	
-	void updateSignalName();
-
-
-	/** Returns the width of the signal
-	 * @return the width of the signal
-	 */	
-	int width() const;
-
-	
-	/** Returns the exponent width of the signal
-	 * @return the width of the exponent if signal is isFP_
-	 */	
-	int wE() const;
-
-	/** Returns the fraction width of the signal
-	 * @return the width of the fraction if signal is isFP_
-	 */	
-	int wF() const;
-	
-	/** Reports if the signal is a floating-point signal
-	 * @return if the signal is a FP siglal
-	 */	
-	bool isFP() const;
-
-	/** Reports if the signal has the bus flag active
-	 * @return true if the signal is of bus type (std_logic_vector)
-	 */		
-	bool isBus() const;
-
-	/** Returns the type of the signal
-	 * @return type of signal, @see SignalType
-	 */	
-	SignalType type() const;
-	
-	/** outputs the VHDL code for declaring this signal 
-	 * @return the VHDL for this signal. 
-	 */	
-	std::string toVHDL();
-
-	/** obtain the name of a signal delayed by delay 
-	 * @param delay*/
-	std::string delayedName(int delay);
-
-
-	/** outputs the VHDL code for declaring a signal with all its delayed versions
-	 * This is the 2.0 equivalent of toVHDL()
-	 * @return the VHDL for the declarations. 
-	 */	
-	std::string toVHDLDeclaration();
-
-	/** Returns a subsignal of the this signal
-	 * @param low the low index of subsignal
-	 * @param high the high index of the subsignal
-	 * @return the corresponding subsignal
-	 */	
-	Signal getSubSignal(int low, int high);
-	
-	/** Returns a subsignal containing the exception bits of this signal
-	 * @return the corresponding subsignal
-	 */	
-	Signal getException();
-
-	/** Returns a subsignal containing the sign bit of this signal
-	 * @return the corresponding subsignal
-	 */	
-	Signal getSign();
-
-	/** Returns a subsignal containing the exponent bits of this signal
-	 * @return the corresponding subsignal
-	 */	
-	Signal getExponent();
-
-
-	/** Returns a subsignal containing the fraction bits of this signal
-	 * @return the corresponding subsignal
-	 */	
-	Signal getMantissa();
-
-
-
-	/** sets the cycle at which the signal is active
-	 */	
-	void setCycle(int cycle) ;
-
-
-	/** obtain the declared cycle of this signal
-	 * @return the cycle
-	 */	
-	int getCycle();
-
-
-	/** Updates the max delay associated to a signal
-	 */	
-	void updateLifeSpan(int delay) ;
-
-
-	/** obtain max delay that has been applied to this signal
-	 * @return the max delay 
-	 */	
-	int getLifeSpan() ;
-
-	/** Set the number of possible output values. */
-	void  setNumberOfPossibleValues(int n);
-
-
-	/** Get the number of possible output values. */
-	int getNumberOfPossibleValues(); 
+namespace flopoco{
 
 	/**
-	 * Converts the value of the signal into a nicely formated VHDL expression,
-	 * including padding and putting quot or apostrophe.
-	 * @param v value
-	 * @param quot also put quotes around the value
-	 * @return a string holding the value in binary
+	 * A class representing a signal. This is the basic block that operators use
 	 */
-	std::string valueToVHDL(mpz_class v, bool quot = true);
+	class Signal
+	{
+	public:
+		/** The possible types of a signal*/
+		typedef enum {
+			in,                        /**< if the signal is an input signal */
+			out,                       /**< if the signal is an output signal */
+			wire,                      /**< if the signal is a wire (not registered) */
+			registeredWithoutReset,    /**< if the signal is registered, but does not have a reset */
+			registeredWithAsyncReset,  /**< if the signal is registered, and has an asynchronous reset */
+			registeredWithSyncReset    /**< if the signal is registered, and has an synchronous reset */
+		} SignalType;
+
+		/** Signal constructor.
+		 * The standard constructor for signals which are not floating-point.
+		 * @param name      the name of the signal
+		 * @param type      the type of the signal, @see SignalType
+		 * @param width     the width of the signal
+		 * @param isBus     the flag which signals if the signal is a bus (std_logic_vector)
+		 */
+		Signal(const std::string name, const Signal::SignalType type, const int width = 1, const bool isBus=false);
+		/** Signal constructor.
+		 * The standard constructor for signals which are floating-point.
+		 * @param name      the name of the signal
+		 * @param type      the type of the signal, @see SignalType
+		 * @param wE        the exponent width
+		 * @param wF        the significand width
+		 */
+		Signal(const std::string name, const SignalType type, const int wE, const int wF);
+
+		/** Signal destructor.
+		 */		
+		~Signal();
 	
-	/**
-	 * Converts the value of the signal into a nicely formated VHDL expression,
-	 * including padding and putting quot or apostrophe. (Hex version.)
-	 * @param v value
-	 * @param quot also put quotes around the value
-	 * @return a string holding the value in hexa
-	 */
-	std::string valueToVHDLHex(mpz_class v, bool quot = true);
+		/** Returns the name of the signal
+		 * @return the name of the signal
+		 */	
+		const std::string& getName() const;
+
+		/** Updates the name of the signal.
+		 * It takes into consideration the fact that we might have subsignals
+		 */	
+		void updateSignalName();
 
 
+		/** Returns the width of the signal
+		 * @return the width of the signal
+		 */	
+		int width() const;
 
-private:
-	std::string   name_;        /**< The name of the signal */
-	std::string   id_;          /**< The id of the signal. It is the same as name_ for regular signals, and is name_(high_-1 downto low_) for subsignals */
-	SignalType    type_;        /**< The type of the signal, see SignalType */
-	int           width_;       /**< The width of the signal */
-
-	int           numberOfPossibleValues_; /**< For signals of type out, indicates how many values will be acceptable. Typically 1 for correct rounding, and 2 for faithful rounding */
-
-	int           lifeSpan_;    /**< The max delay that will be applied to this signal; */
-	int           cycle_;       /**<  the cycle at which this signal is active in a pipelined operator. 0 means synchronized with the inputs */
 	
-	bool          isFP_;        /**< If the signal is of floating-point type */  
-	int           wE_;          /**< The width of the exponent. Used for FP signals */
-	int           wF_;          /**< The width of the fraction. Used for FP signals */
+		/** Returns the exponent width of the signal
+		 * @return the width of the exponent if signal is isFP_
+		 */	
+		int wE() const;
+
+		/** Returns the fraction width of the signal
+		 * @return the width of the fraction if signal is isFP_
+		 */	
+		int wF() const;
 	
-	bool          isSubSignal_; /**< If the signal is a subsignal */
-	int           low_;         /**< The low index of the signal */
-	int           high_;        /**< The high index of the signal */
+		/** Reports if the signal is a floating-point signal
+		 * @return if the signal is a FP siglal
+		 */	
+		bool isFP() const;
+
+		/** Reports if the signal has the bus flag active
+		 * @return true if the signal is of bus type (std_logic_vector)
+		 */		
+		bool isBus() const;
+
+		/** Returns the type of the signal
+		 * @return type of signal, @see SignalType
+		 */	
+		SignalType type() const;
 	
-	bool          isBus_;       /**< True is the signal is a bus (std_logic_vector)*/
-};
+		/** outputs the VHDL code for declaring this signal 
+		 * @return the VHDL for this signal. 
+		 */	
+		std::string toVHDL();
+
+		/** obtain the name of a signal delayed by delay 
+		 * @param delay*/
+		std::string delayedName(int delay);
+
+
+		/** outputs the VHDL code for declaring a signal with all its delayed versions
+		 * This is the 2.0 equivalent of toVHDL()
+		 * @return the VHDL for the declarations. 
+		 */	
+		std::string toVHDLDeclaration();
+
+		/** Returns a subsignal of the this signal
+		 * @param low the low index of subsignal
+		 * @param high the high index of the subsignal
+		 * @return the corresponding subsignal
+		 */	
+		Signal getSubSignal(int low, int high);
+	
+		/** Returns a subsignal containing the exception bits of this signal
+		 * @return the corresponding subsignal
+		 */	
+		Signal getException();
+
+		/** Returns a subsignal containing the sign bit of this signal
+		 * @return the corresponding subsignal
+		 */	
+		Signal getSign();
+
+		/** Returns a subsignal containing the exponent bits of this signal
+		 * @return the corresponding subsignal
+		 */	
+		Signal getExponent();
+
+
+		/** Returns a subsignal containing the fraction bits of this signal
+		 * @return the corresponding subsignal
+		 */	
+		Signal getMantissa();
+
+
+
+		/** sets the cycle at which the signal is active
+		 */	
+		void setCycle(int cycle) ;
+
+
+		/** obtain the declared cycle of this signal
+		 * @return the cycle
+		 */	
+		int getCycle();
+
+
+		/** Updates the max delay associated to a signal
+		 */	
+		void updateLifeSpan(int delay) ;
+
+
+		/** obtain max delay that has been applied to this signal
+		 * @return the max delay 
+		 */	
+		int getLifeSpan() ;
+
+		/** Set the number of possible output values. */
+		void  setNumberOfPossibleValues(int n);
+
+
+		/** Get the number of possible output values. */
+		int getNumberOfPossibleValues(); 
+
+		/**
+		 * Converts the value of the signal into a nicely formated VHDL expression,
+		 * including padding and putting quot or apostrophe.
+		 * @param v value
+		 * @param quot also put quotes around the value
+		 * @return a string holding the value in binary
+		 */
+		std::string valueToVHDL(mpz_class v, bool quot = true);
+	
+		/**
+		 * Converts the value of the signal into a nicely formated VHDL expression,
+		 * including padding and putting quot or apostrophe. (Hex version.)
+		 * @param v value
+		 * @param quot also put quotes around the value
+		 * @return a string holding the value in hexa
+		 */
+		std::string valueToVHDLHex(mpz_class v, bool quot = true);
+
+
+
+	private:
+		std::string   name_;        /**< The name of the signal */
+		std::string   id_;          /**< The id of the signal. It is the same as name_ for regular signals, and is name_(high_-1 downto low_) for subsignals */
+		SignalType    type_;        /**< The type of the signal, see SignalType */
+		int           width_;       /**< The width of the signal */
+
+		int           numberOfPossibleValues_; /**< For signals of type out, indicates how many values will be acceptable. Typically 1 for correct rounding, and 2 for faithful rounding */
+
+		int           lifeSpan_;    /**< The max delay that will be applied to this signal; */
+		int           cycle_;       /**<  the cycle at which this signal is active in a pipelined operator. 0 means synchronized with the inputs */
+	
+		bool          isFP_;        /**< If the signal is of floating-point type */  
+		int           wE_;          /**< The width of the exponent. Used for FP signals */
+		int           wF_;          /**< The width of the fraction. Used for FP signals */
+	
+		bool          isSubSignal_; /**< If the signal is a subsignal */
+		int           low_;         /**< The low index of the signal */
+		int           high_;        /**< The high index of the signal */
+	
+		bool          isBus_;       /**< True is the signal is a bus (std_logic_vector)*/
+	};
+
+}
 
 #endif
 
