4564d52753f0bdbb6ea03b4aa85ff2539282f799
Mioara Joldes <no@available>
2010-02-16 11:27:58 +0000

    fpminimax command added

--- src/HOTBM/sollya.h
+++ src/HOTBM/sollya.h
@@ -55,6 +55,50 @@ knowledge of the CeCILL-C license and that you accept its terms.
 #ifdef __cplusplus
 extern "C" {
 #endif
+#define VARIABLE 0
+#define CONSTANT 1
+#define ADD 2
+#define SUB 3
+#define MUL 4
+#define DIV 5
+#define SQRT 6
+#define EXP 7
+#define LOG 8
+#define LOG_2 9
+#define LOG_10 10
+#define SIN 11
+#define COS 12
+#define TAN 13
+#define ASIN 14
+#define ACOS 15
+#define ATAN 16
+#define SINH 17
+#define COSH 18
+#define TANH 19
+#define ASINH 20
+#define ACOSH 21
+#define ATANH 22
+#define POW 23
+#define NEG 24
+#define ABS 25
+#define DOUBLE 26
+#define DOUBLEDOUBLE 27
+#define TRIPLEDOUBLE 28
+#define POLYNOMIAL 29
+#define ERF 30
+#define ERFC 31
+#define LOG_1P 32
+#define EXP_M1 33
+#define DOUBLEEXTENDED 34
+#define LIBRARYFUNCTION 35
+#define CEIL 36
+#define FLOOR 37
+#define PI_CONST 38
+#define FIXED 236
+#define FLOATING 237
+#define ABSOLUTESYM 197
+#define RELATIVESYM 198
+#define SINGLE 39
 
 typedef struct sollya_chain* sollya_chain_t;
 typedef struct sollya_node* sollya_node_t;
@@ -160,6 +204,10 @@ sollya_chain_t fpFindZerosFunction(sollya_node_t func, sollya_range_t range, mp_
 
 sollya_node_t remez(sollya_node_t func, sollya_node_t weight, sollya_chain_t monom, mpfr_t a, mpfr_t b, mpfr_t *requestedQuality, mp_prec_t prec);
 sollya_range_t guessDegree(sollya_node_t func, sollya_node_t weight, mpfr_t a, mpfr_t b, mpfr_t eps);
+
+sollya_node_t FPminimax(sollya_node_t f,	sollya_chain_t monomials, sollya_chain_t formats,  sollya_chain_t points, mpfr_t a, mpfr_t b, int fp, int absrel, sollya_node_t consPart, sollya_node_t minimax);
+
+
 void uncertifiedInfnorm(mpfr_t result, sollya_node_t tree, mpfr_t a, mpfr_t b, unsigned long int points, mp_prec_t prec);
 
 sollya_node_t taylor(sollya_node_t tree, int degree, sollya_node_t point, mp_prec_t prec);
