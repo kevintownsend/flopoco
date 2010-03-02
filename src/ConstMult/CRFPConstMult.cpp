b54887122252a1b319afdcf80969b3b298113462
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2010-03-02 15:32:22 +0000

    License cleanup

--- src/ConstMult/CRFPConstMult.cpp
+++ src/ConstMult/CRFPConstMult.cpp
@@ -1,24 +1,15 @@
 /*
- * A correctly-rounded multiplier by an arbitrary real constant for FloPoCo
- *
- * Author : Florent de Dinechin
- *
- * This file is part of the FloPoCo project developed by the Arenaire
- * team at Ecole Normale Superieure de Lyon
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+  A correctly-rounded multiplier by an arbitrary real constant for FloPoCo
+ 
+  This file is part of the FloPoCo project developed by the Arenaire
+  team at Ecole Normale Superieure de Lyon
+  
+  Author : Florent de Dinechin, Florent.de.Dinechin@ens-lyon.fr
+
+  Initial software.
+  Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
+  CeCILL license, 2008-2010.
+
 */
 
 #ifdef HAVE_SOLLYA
--- src/ConstMult/LastKCMTable.cpp
+++ src/ConstMult/LastKCMTable.cpp
@@ -1,3 +1,15 @@
+/*
+  This file is part of the FloPoCo project developed by the Arenaire
+  team at Ecole Normale Superieure de Lyon
+  
+  Author : Florent de Dinechin, Florent.de.Dinechin@ens-lyon.fr
+
+  Initial software.
+  Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
+  CeCILL License, 2008-2010.
+*/
+
+
 #include <iostream>
 #include <math.h>
 #include <cstdlib>
