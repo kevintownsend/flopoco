6b0fb01db418c9e78100dc0298aefed1a1eeb893
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2010-03-16 06:21:09 +0000

    licence update

--- src/IntTruncMultiplier.cpp
+++ src/IntTruncMultiplier.cpp
@@ -1,25 +1,15 @@
 /*
- * An integer multiplier for FloPoCo
- *
- * Authors : Bogdan Pasca
- *
- * This file is part of the FloPoCo project developed by the Arenaire
- * team at Ecole Normale Superieure de Lyon
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
- */
+  Truncated Integer Multiplier for FloPoCo
+ 
+  This file is part of the FloPoCo project 
+  developed by the Arenaire team at Ecole Normale Superieure de Lyon
+  
+ * Authors : Sebastian Banescu, Bogdan Pasca, Radu Tudoran
+
+  Initial software.
+  Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
+  CeCILL license, 2008-2010.
+*/
 
 #include <typeinfo>
 #include <iostream>
