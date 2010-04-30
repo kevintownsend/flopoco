03650bc19f8d432726f3755147afe1826c726ab5
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2010-04-30 12:17:17 +0000

    TestBenchFile is not yet ready for prime-time

--- tools/release_test.py
+++ tools/release_test.py
@@ -11,7 +11,7 @@ import time
 
 test_cases_per_combination = 10000
 useModelSim=True # if True, use modelsim; if False, use ghdl
-testBench = "TestBenchFile"   #one of TestBench or TestBenchFile
+testBench = "TestBench"   #one of TestBench or TestBenchFile
 
 
 #-------------------------------------------------------------------------------
