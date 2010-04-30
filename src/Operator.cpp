06f512b7002bb5cff07bce4fdc68b622268071b8
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2010-04-30 16:33:06 +0000

    Tobin tax change

--- src/Operator.cpp
+++ src/Operator.cpp
@@ -1,24 +1,12 @@
 /*
- * The base Operator class, every operator should inherit it
- *
- * Author : Florent de Dinechin
- *
- * This file is part of the FloPoCo project developed by the Arenaire
- * team at Ecole Normale Superieure de Lyon
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+  The base Operator class, every operator should inherit it
+
+  Author : Florent de Dinechin, Bogdan Pasca
+
+  Initial software.
+  Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
+  CeCILL license, 2008-2010.
+ 
  */
 
 
@@ -338,7 +326,8 @@ namespace flopoco{
 		}
 
 		o<<"-- This operator is part of the Infinite Virtual Library FloPoCoLib"<<endl
-		 <<"-- and is distributed under the terms of the GNU Lesser General Public Licence."<<endl
+		 <<"-- and is distributed under the terms of the GNU Lesser General Public Licence"<<endl
+		 <<"-- with a Tobin Tax restriction (see README file for details)."<<endl
 		 <<"-- Authors: " << authorsyears <<endl
 		 <<"--------------------------------------------------------------------------------"<<endl;
 
