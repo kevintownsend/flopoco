f43b4acbeac3b8067d771d9865fd9a08ada799e5
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2010-05-06 05:00:15 +0000

    Added comments and changed license

--- src/PolynomialEvaluator.cpp
+++ src/PolynomialEvaluator.cpp
@@ -1,24 +1,16 @@
 /*
- * A polynomial evaluator for FloPoCo
- *
- * Author : Bogdan Pasca
- *
- * This file is part of the FloPoCo project developed by the Arenaire
- * team at Ecole Normale Superieure de Lyon
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or 
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+  A polynomial evaluator for FloPoCo
+ 
+  Author : Bogdan Pasca
+ 
+  This file is part of the FloPoCo project 
+  developed by the Arenaire team at Ecole Normale Superieure de Lyon
+  
+  Authors:   Bogdan Pasca, Florent de Dinechin
+
+  Initial software.
+  Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
+  CeCILL license, 2008-2010.
 */
 
 #include <iostream>
@@ -203,17 +195,19 @@ namespace flopoco{
 
 
 		ostringstream s1, s2;		
-		
+		s1 << "Guard bits for the sums: ";
+		s2 << "Gard bits on the truncation of y : ";
 		for (int j=0; j<=degree_; j++)
 			s1 << "aG["<<j<<"]="<<aGuard_[j]<<" "; 
 
 		for (int j=1; j<=degree_; j++)
 			s2 << "yG["<<j<<"]="<<yGuard_[j]<<" "; 
-			
-		REPORT(INFO, "------------------------------------------------------------");
+		
+		// Commented out by Florent whose laptop has a smaller screen
+		//REPORT(INFO, "------------------------------------------------------------");
 		REPORT(INFO, s1.str());
 		REPORT(INFO, s2.str());
-		REPORT(INFO, "------------------------------------------------------------");
+		//REPORT(INFO, "------------------------------------------------------------");
 
 
 //		exit(-1);///////////////////////////////
