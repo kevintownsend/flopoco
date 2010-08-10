ffe89585979998598c6b5809870a8be1f21e615d
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2010-08-10 19:46:50 +0000

    tests for power

--- tools/tests/FPExp.cmd
+++ tools/tests/FPExp.cmd
@@ -1,3 +1,5 @@
 #   FPExp wE wF
 flopoco FPExp 8 23
 flopoco FPExp 11 52
+flopoco FPExp 9 25
+flopoco FPExp 10 35
---
+++ tools/tests/FPPowr.cmd
@@ -0,0 +1,3 @@
+#   FPPowr wE wF LogTableSize ExpTableSize ExpDegree
+flopoco FPExp 8 23
+flopoco FPExp 11 52
