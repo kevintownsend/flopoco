01d64a38bdb18368377c5c02f2401969f1726796
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2010-11-17 15:15:10 +0000

    improved FPMultiplierTiling

--- src/FPMultiplierTiling.cpp
+++ src/FPMultiplierTiling.cpp
@@ -1,16 +1,16 @@
 /*
-  Floating Point Multiplier for FloPoCo
- 
-  This file is part of the FloPoCo project
-  developed by the Arenaire team at Ecole Normale Superieure de Lyon
-  
-  Author: Bogdan Pasca
+Floating Point Multiplier for FloPoCo
 
-  Initial software.
-  Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
-  CeCILL license, 2008-2010.
+This file is part of the FloPoCo project
+developed by the Arenaire team at Ecole Normale Superieure de Lyon
 
- */
+Author: Bogdan Pasca
+
+Initial software.
+Copyright © ENS-Lyon, INRIA, CNRS, UCBL,  
+CeCILL license, 2008-2010.
+
+*/
 
 #include <iostream>
 #include <sstream>
@@ -28,20 +28,20 @@
 using namespace std;
 
 namespace flopoco{
-
+	
 	extern vector<Operator*> oplist;
-
+	
 	FPMultiplierTiling::FPMultiplierTiling(Target* target, int wEX, int wFX, int wEY, int wFY, int wER, int wFR, int norm,float ratio, int maxTimeInMinutes) :
-		Operator(target), wEX_(wEX), wFX_(wFX), wEY_(wEY), wFY_(wFY), wER_(wER), wFR_(wFR) {
-
+	Operator(target), wEX_(wEX), wFX_(wFX), wEY_(wEY), wFY_(wFY), wER_(wER), wFR_(wFR) {
+		
 		ostringstream name;
 		name << "FPMultiplierTiling_"<<wEX_<<"_"<<wFX_<<"_"<<wEY_<<"_"<<wFY_<<"_"<<wER_<<"_"<<wFR_; 
 		setName(name.str());
-
-
+		
+		
 		/* set if operator outputs a normalized_ result */
 		normalized_ = (norm==0?false:true);
-	
+		
 		addFPInput ("X", wEX_, wFX_);
 		addFPInput ("Y", wEY_, wFY_);
 		if(normalized_==true) 
@@ -53,33 +53,33 @@ namespace flopoco{
 			addOutput ("ResultException"  , 2      );
 			addOutput ("ResultSign"       , 1      ); 
 		}
-
+		
 		/* Sing Handling */
 		vhdl << tab << declare("sign") << " <= X" << of(wEX_+wFX) << " xor Y" << of(wEY_+wFY) << ";" << endl;
-
+		
 		/* Exponent Handling */
 		vhdl << tab << declare("expX", wEX_) << " <= X"<< range(wEX_ + wFX_ -1, wFX_) << ";" << endl; 
 		vhdl << tab << declare("expY", wEY_) << " <= Y"<< range(wEY_ + wFY_ -1, wFX_) << ";" << endl; 
-
+		
 		//Add exponents and substract bias
 		vhdl << tab << declare("expSumPreSub", wEX_+2) << " <= (\"00\" & expX) + (\"00\" & expY);" << endl; 
 		nextCycle(); ///////////////////////////////////////////////////////
 		vhdl << tab << declare("bias", wEX_+2) << " <= CONV_STD_LOGIC_VECTOR(" << intpow2(wER-1)-1 << ","<<wEX_+2<<");"<< endl; 
 		vhdl << tab << declare("expSum",wEX+2) << " <= expSumPreSub - bias;" << endl;   
-	
+		
 		/* Significand Handling */
 		setCycle(0); 
 		vhdl << tab << declare("sigX",1 + wFX_) << " <= \"1\" & X" << range(wFX_-1,0) << ";" << endl;
 		vhdl << tab << declare("sigY",1 + wFY_) << " <= \"1\" & Y" << range(wFY_-1,0) << ";" << endl;
-
+		
 		intmult_ = new IntTilingMult(target, wFX_+1, wFY_+1, ratio, maxTimeInMinutes,false);
 		oplist.push_back(intmult_);
-
+		
 		inPortMap( intmult_, "X", "sigX");
 		inPortMap( intmult_, "Y", "sigY");
 		outPortMap(intmult_, "R", "sigProd");
 		vhdl << instance(intmult_, "SignificandMultiplication");
-
+		
 		/* Exception Handling */
 		setCycle(0);
 		vhdl << tab << declare("excSel",4) <<" <= X"<<range(wEX_ + wFX_ +2, wEX_ + wFX_ + 1) << " & Y"<<range(wEY_ + wFY_ +2, wEY_ + wFY_ +1) << ";" << endl;
@@ -89,53 +89,65 @@ namespace flopoco{
 		vhdl << tab << "       \"01\" when \"0101\","<<endl;
 		vhdl << tab << "       \"10\" when \"0110\" | \"1001\" | \"1010\" ,"<<endl;		
 		vhdl << tab << "       \"11\" when others;"<<endl;		
-
+		
 		//syncronization
 		syncCycleFromSignal("sigProd");
 		syncCycleFromSignal("expSum");
-		nextCycle();///////////////////////////////	
-		
+		nextCycle();/////////////////////////////// -> this one we may need as IntTilingMult is not flopoco 2.0 compatible	
+		setCriticalPath(0.0);
 		if (normalized_==true){
-		/******************************************************************/
-				
+			/******************************************************************/
+			
 			vhdl << tab<< declare("norm") << " <= sigProd" << of(wFX_+wFY_+1) << ";"<<endl;
+			double cpnorm = getCriticalPath();
+			manageCriticalPath(target->localWireDelay() + target->adderDelay(wEX_+2));
 			vhdl << tab<< declare("expPostNorm", wEX_+2) << " <= expSum + (" << zg(wEX_+1,0) << " & norm);"<<endl;
-
+			
 			//check is rounding is needed
 			if (1+wFR_ >= wFX_+wFY_+2) {  /* => no rounding needed - possible padding */
-				vhdl << tab << declare("resSig", wFR_) << " <= sigProd" << range(wFX_+wFY_,0) << " & " <<   zg(1+wFR_ - (wFX_+wFY_+2) , 0)<<" when norm='1' else"<<endl;
-				vhdl << tab <<"                      sigProd" << range(wFX_+wFY_-1,0) << " & " << zg(1+wFR_ - (wFX_+wFY_+2) + 1 , 0) << ";"<<endl;
-
+				manageCriticalPath(target->localWireDelay()+ target->lutDelay() );
 				vhdl << tab <<"with expPostNorm" << range(wER_+1, wER_) << " select"<<endl;		
 				vhdl << tab << declare("excPostNorm",2) << " <=  \"01\"  when  \"00\","<<endl;
 				vhdl << tab <<"                            \"10\"             when \"01\", "<<endl;
 				vhdl << tab <<"                            \"00\"             when \"11\"|\"10\","<<endl;
-				vhdl << tab <<"                            \"11\"             when others;"<<endl;						
-				
+				vhdl << tab <<"                            \"11\"             when others;"<<endl;
+				manageCriticalPath(target->localWireDelay()+ target->lutDelay() );
 				vhdl << tab << "with exc select " << endl;
 				vhdl << tab << declare("finalExc",2) << " <= exc when  \"11\"|\"10\"|\"00\"," <<endl;
 				vhdl << tab << "                    excPostNorm when others; " << endl;
-		
+				double cpfinalExc =getCriticalPath();
+				
+				syncCycleFromSignal("norm");
+				setCriticalPath(cpnorm);
+				manageCriticalPath(target->localWireDelay() + target->lutDelay()); 
+				vhdl << tab << declare("resSig", wFR_) << " <= sigProd" << range(wFX_+wFY_,0) << " & " <<   zg(1+wFR_ - (wFX_+wFY_+2) , 0)<<" when norm='1' else"<<endl;
+				vhdl << tab <<"                      sigProd" << range(wFX_+wFY_-1,0) << " & " << zg(1+wFR_ - (wFX_+wFY_+2) + 1 , 0) << ";"<<endl;
+				
+				if (syncCycleFromSignal("finalExc")){
+					if (getCycleFromSignal("finalExc") == getCycleFromSignal("resSig"))
+						setCriticalPath(max( getCriticalPath(), cpfinalExc));
+				}
 				vhdl << tab << "R <= finalExc & sign & expPostNorm" << range(wER_-1, 0) << " & resSig;"<<endl;
 			}
-			else{
-				nextCycle();////
-//				vhdl << tab << declare("resSig", wFR_) << " <= sigProd" << range(wFX_+wFY_, wFX_+wFY_ - wFR_+1) <<" when norm='1' else"<<endl;
-//				vhdl << tab <<"                      sigProd" << range(wFX_+wFY_-1, wFX_+wFY_ - wFR_) << ";"<<endl;
-
+			else{ // ROUNDING NEEDED
+				syncCycleFromSignal("norm");
+				setCriticalPath(cpnorm);
+				manageCriticalPath(target->localWireDelay()+ target->lutDelay() );
 				vhdl << tab << declare("sigProdExt", wFX_+wFY_+ 1 + 1) << " <= sigProd" << range(wFX_+wFY_, 0) << " & " << zg(1,0) <<" when norm='1' else"<<endl;
 				vhdl << tab << "                      sigProd" << range(wFX_+wFY_-1, 0) << " & " << zg(2,0) << ";"<<endl;
-							
+				
 				vhdl << tab << declare("expSig", 2 + wER_ + wFR_) << " <= expPostNorm & sigProdExt" << range(wFX_+wFY_+ 1,wFX_+wFY_+ 2 -wFR_) << ";" << endl;
-
-		
+				
 				vhdl << tab << declare("sticky") << " <= sigProdExt" << of(wFX_+wFY + 1 - wFR) << ";" << endl;
+				
+				manageCriticalPath(target->localWireDelay() + target->comparatorConstDelay(wFX_+wFY + 1 - wFR));
 				vhdl << tab << declare("guard") << " <= '0' when sigProdExt" << range(wFX_+wFY + 1 - wFR - 1,0) << "=" << zg(wFX_+wFY + 1 - wFR - 1 +1,0) <<" else '1';" << endl;
+				
+				manageCriticalPath(target->localWireDelay() + target->lutDelay());
 				vhdl << tab << declare("round") << " <= sticky and ( (guard and not(sigProdExt" << of(wFX_+wFY + 1 - wFR+1) <<")) or (" 
-				 << "sigProdExt" << of(wFX_+wFY + 1 - wFR+1) << " ))  ;" << endl;
-				                                                                      
-				nextCycle();////			
-				intadd_ = new IntAdder(target, 2 + wER_ + wFR_);
+				<< "sigProdExt" << of(wFX_+wFY + 1 - wFR+1) << " ))  ;" << endl;
+				
+				intadd_ = new IntAdder(target, 2 + wER_ + wFR_, inDelayMap("X", getCriticalPath()));
 				oplist.push_back(intadd_);
 				
 				inPortMap    (intadd_, "X",   "expSig");
@@ -145,41 +157,44 @@ namespace flopoco{
 				
 				vhdl << tab << instance( intadd_, "RoundingAdder");
 				syncCycleFromSignal("expSigPostRound");
-				nextCycle();
+				setCriticalPath(intadd_->getOutputDelay("R"));
+				
+				manageCriticalPath(target->localWireDelay() + target->lutDelay());
 				vhdl << tab <<"with expSigPostRound" << range(wER_+wFR_+1, wER_+wFR_) << " select"<<endl;		
 				vhdl << tab << declare("excPostNorm",2) << " <=  \"01\"  when  \"00\","<<endl;
 				vhdl << tab <<"                            \"10\"             when \"01\", "<<endl;
 				vhdl << tab <<"                            \"00\"             when \"11\"|\"10\","<<endl;
 				vhdl << tab <<"                            \"11\"             when others;"<<endl;						
-			
+				
+				manageCriticalPath(target->localWireDelay() + target->lutDelay());
 				vhdl << tab << "with exc select " << endl;
 				vhdl << tab << declare("finalExc",2) << " <= exc when  \"11\"|\"10\"|\"00\"," <<endl;
 				vhdl << tab << "                    excPostNorm when others; " << endl;
-	
+				
 				vhdl << tab << "R <= finalExc & sign & expSigPostRound" << range(wER_+wFR_-1, 0)<<";"<<endl;
-			
 			}
 		}else{ //the non-normalized version for DotProduct
-				vhdl << tab <<"with expSum" << range(wER_+1, wER_) << " select"<<endl;		
-				vhdl << tab << declare("excPostProc",2) << " <=  \"01\"  when  \"00\","<<endl;
-				vhdl << tab <<"                            \"10\"             when \"01\", "<<endl;
-				vhdl << tab <<"                            \"00\"             when \"11\"|\"10\","<<endl;
-				vhdl << tab <<"                            \"11\"             when others;"<<endl;						
-				
-				vhdl << tab << "with exc select " << endl;
-				vhdl << tab << declare("finalExc",2) << " <= exc when  \"11\"|\"10\"|\"00\"," <<endl;
-				vhdl << tab << "                    excPostProc when others; " << endl;
-				
-				vhdl << tab << "ResultExponent <= expSum" << range(wER_-1, 0) << ";" << endl;
-				vhdl << tab << "ResultSignificand <= sigProd;" << endl;
-				vhdl << tab << "ResultException <= finalExc;" << endl;
-				vhdl << tab << "ResultSign <= sign;" << endl;
+			//TODO IF anyone needs it sometime
+			vhdl << tab <<"with expSum" << range(wER_+1, wER_) << " select"<<endl;		
+			vhdl << tab << declare("excPostProc",2) << " <=  \"01\"  when  \"00\","<<endl;
+			vhdl << tab <<"                            \"10\"             when \"01\", "<<endl;
+			vhdl << tab <<"                            \"00\"             when \"11\"|\"10\","<<endl;
+			vhdl << tab <<"                            \"11\"             when others;"<<endl;						
+			
+			vhdl << tab << "with exc select " << endl;
+			vhdl << tab << declare("finalExc",2) << " <= exc when  \"11\"|\"10\"|\"00\"," <<endl;
+			vhdl << tab << "                    excPostProc when others; " << endl;
+			
+			vhdl << tab << "ResultExponent <= expSum" << range(wER_-1, 0) << ";" << endl;
+			vhdl << tab << "ResultSignificand <= sigProd;" << endl;
+			vhdl << tab << "ResultException <= finalExc;" << endl;
+			vhdl << tab << "ResultSign <= sign;" << endl;
 		}
 	} // end constructor
-
+	
 	FPMultiplierTiling::~FPMultiplierTiling() {
 	}
-
+	
 	// FIXME: the following is only functional for a correctly rounded multiplier.
 	// The old code for the non-normalized case is commented out below, just in case.
 	void FPMultiplierTiling::emulate(TestCase * tc)
@@ -187,7 +202,7 @@ namespace flopoco{
 		/* Get I/O values */
 		mpz_class svX = tc->getInputValue("X");
 		mpz_class svY = tc->getInputValue("Y");
-
+		
 		/* Compute correct value */
 		FPNumber fpx(wEX_, wFX_), fpy(wEY_, wFY_);
 		fpx = svX;
@@ -199,15 +214,15 @@ namespace flopoco{
 		fpx.getMPFR(x);
 		fpy.getMPFR(y);
 		mpfr_mul(r, x, y, GMP_RNDN);
-
+		
 		// Set outputs 
 		FPNumber  fpr(wER_, wFR_, r);
 		mpz_class svR = fpr.getSignalValue();
 		tc->addExpectedOutput("R", svR);
-
+		
 		// clean up
 		mpfr_clears(x, y, r, NULL);
 	}
-
-
+	
+	
 }
