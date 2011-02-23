d133eecb961a55de0bc521154b523c727b37d000
Bogdan Pasca <bogdan.mihai.pasca@gmail.com>
2011-02-23 12:23:01 +0000

    added -n option for new release test generation. no parameter contiunes from where it left-off

--- tools/release_test.py
+++ tools/release_test.py
@@ -6,13 +6,13 @@ import string
 import commands
 import pipes
 import time
-
+import sys
+import fileinput
 
 
 test_cases_per_combination = 1000
 useModelSim=True # if True, use modelsim; if False, use ghdl
 testBench = "TestBenchFile"   #one of TestBench or TestBenchFile
-
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
@@ -34,154 +34,193 @@ def printResults(list, lfile):
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
 
+def pre_append(line, file_name):
+    fobj = fileinput.FileInput(file_name, inplace=1)
+    first_line = fobj.readline()
+    sys.stdout.write("%s\n%s" % (line, first_line))
+    for line in fobj:
+        sys.stdout.write("%s" % line)
+    fobj.close()
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
-res = []
-#REMOVE TEMPORARY MODELSIM FILES
-os.system("rm -f wlf*")
 
-#CREATE A LOG FILE 
-logfile = open( "release_test.log","w")
 
-pass_all = True
-#PARSE EXTERNAL TEST FILE
-print("Parsing operators from external list: \n")
-logfile.write("Parsing operators from external list: \n")
+#/* main */
+if __name__ == '__main__':
 
-for filename in os.listdir("tests/"):
-	if ((filename[len(filename)-1]!='~') and (filename[0]!='.')):
-		print "processing " + filename + " ... "
+	if (len(sys.argv)==2):
+		option = sys.argv[1] #if -n we do a clean test
+	else:
+		print "-n option is for starting a new regression test. all results will be cleared\n"
+		option = ""
+	res = []
+	#REMOVE TEMPORARY MODELSIM FILES
+	os.system("rm -f wlf*")
+
+	#CREATE A LOG FILE 
+	if ( option == "-n" ) :
+		logfile = open( "release_test.log","w")
+		logfile.close()
+
+	logfile = open( "release_test.log","a")
 		
-		fd = open ("tests/"+filename)
-		if fd < 0:
-			print "Unable to open file ".filename
-			logfile.write("Unable to open file " + filename)
-
-		skipfile = False
-		for line in fd:
-			if (line[0]=='!'):
-				skipfile = True
-				print "Skipping the rest of the file "+filename+"; ! encountered"
-				
-			if ((line[0]!='#') and (len(line)>1) and (skipfile == False)):
-				run_cmd = line[:len(line)-1] + " " + testBench + " " + `test_cases_per_combination`
-				print run_cmd
-				logfile.write(run_cmd+"\n")
-				modelsim_food = commands.getoutput(run_cmd)
-				did_generate_vhdl = True
-				status = string.find(modelsim_food, "Output file: flopoco.vhdl")
-
-				if status < 0:
-					did_generate_vhdl = False
-					print("Did not generate VHDL");
-
-				if useModelSim:
-					commands.getoutput("rm -f vsim*")
-					commands.getoutput("killall -9 vsimk")
-					modelsim_food = modelsim_food[string.find(modelsim_food, "vdel") : string.find(modelsim_food, "To run the simulation using gHDL,")-1 ]
-				else:
-					ghdl_food = modelsim_food[string.find(modelsim_food, "   ghdl") : string.find(modelsim_food, "Final")-1 ]
+	if ( option == "-n" ): 
+		resultfile = open( "release_test.res", "w")
+		resultfile.close()
 	
-				finished = False
-				pass_test = True
-				did_compile = True
+	pass_all = True
+	#PARSE EXTERNAL TEST FILE
+	print("Parsing operators from external list: \n")
+	logfile.write("Parsing operators from external list: \n")
 
-				if did_generate_vhdl:
-					print("It did generate VHDL");
+	for filename in os.listdir("tests/"):
+		if ((filename[len(filename)-1]!='~') and (filename[0]!='.')):
+			print "processing " + filename + " ... "
+		
+			fd = open ("tests/"+filename)
+			if fd < 0:
+				print "Unable to open file ".filename
+				logfile.write("Unable to open file " + filename)
+
+			completedFile = False
+			skipfile = False
+			for line in fd:
+				if ((line[0]=='@') and (option!="-n")):
+					completedFile = True
+					print "Skipping this file. It was completed before the crash"
+
+				if (line[0]=='!'):
+					skipfile = True
+					print "Skipping the rest of the file "+filename+"; ! encountered"
+			
+				if ((line[0]!='#') and (len(line)>1) and (skipfile == False) and (completedFile == False)):
+					resultfile = open( "release_test.res", "a")
+
+					run_cmd = line[:len(line)-1] + " " + testBench + " " + `test_cases_per_combination`
+					print run_cmd
+					logfile.write(run_cmd+"\n")
+					modelsim_food = commands.getoutput(run_cmd)
+					did_generate_vhdl = True
+					status = string.find(modelsim_food, "Output file: flopoco.vhdl")
+
+					if status < 0:
+						did_generate_vhdl = False
+						print("Did not generate VHDL");
 
 					if useModelSim:
-					#start modelsim
-						p = subprocess.Popen("vsim -c", shell=True, bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-						(child_stdin, child_stdout, child_stderr) = (p.stdin, p.stdout, p.stderr)
-						child_stdin.write("vdel -all -lib work\n")
-						child_stdin.write('vlib work\n')
-						child_stdin.write('vcom flopoco.vhdl \n')
-						child_stdin.write('vcom flopoco.vhdl \n')
-						child_stdin.write(modelsim_food[string.find(modelsim_food,"vsim"):string.find(modelsim_food,"add")-1]+"\n" )
-						child_stdin.write('add wave -r * \n')
-						child_stdin.write(modelsim_food[string.find(modelsim_food,"run"):]+"\n")
-						child_stdin.write('exit \n')
-						while ((not finished) and (did_compile)):
-							st = child_stdout.readline()
-							#print st[0:len(st)-2]
-							logfile.write(st[0:len(st)-2]+"\n")
-							status = string.find(st, "Error:")
-							if status > 0:
-								pass_test = False
-								#did_compile = False
-						
-								status = string.find(st, "Incorrect")
+						commands.getoutput("rm -f vsim*")
+						commands.getoutput("killall -9 vsimk")
+						modelsim_food = modelsim_food[string.find(modelsim_food, "vdel") : string.find(modelsim_food, "To run the simulation using gHDL,")-1 ]
+					else:
+						ghdl_food = modelsim_food[string.find(modelsim_food, "   ghdl") : string.find(modelsim_food, "Final")-1 ]
+	
+					finished = False
+					pass_test = True
+					did_compile = True
+
+					if did_generate_vhdl:
+						print("It did generate VHDL");
+
+						if useModelSim:
+						#start modelsim
+							p = subprocess.Popen("vsim -c", shell=True, bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+							(child_stdin, child_stdout, child_stderr) = (p.stdin, p.stdout, p.stderr)
+							child_stdin.write("vdel -all -lib work\n")
+							child_stdin.write('vlib work\n')
+							child_stdin.write('vcom flopoco.vhdl \n')
+							child_stdin.write('vcom flopoco.vhdl \n')
+							child_stdin.write(modelsim_food[string.find(modelsim_food,"vsim"):string.find(modelsim_food,"add")-1]+"\n" )
+							child_stdin.write('add wave -r * \n')
+							child_stdin.write(modelsim_food[string.find(modelsim_food,"run"):]+"\n")
+							child_stdin.write('exit \n')
+							while ((not finished) and (did_compile)):
+								st = child_stdout.readline()
+								#print st[0:len(st)-2]
+								logfile.write(st[0:len(st)-2]+"\n")
+								status = string.find(st, "Error:")
 								if status > 0:
 									pass_test = False
+									#did_compile = False
+						
+									status = string.find(st, "Incorrect")
+									if status > 0:
+										pass_test = False
 		
-							status = string.find(st, "Stopped")
-							if status > 0:
-								finished = True
-								#did_compile = False
-		
-								status = string.find(st, "Error loading design")
+								status = string.find(st, "Stopped")
 								if status > 0:
-									did_compile = False
 									finished = True
-
-						if did_compile:
-							child_stdin.write('exit \n')
-							child_stdout.close()
-							child_stdin.close()
-							child_stderr.close()
-
-					else: # use ghdl
-
-						cmd=ghdl_food[string.find(ghdl_food,"ghdl -a"):string.find(ghdl_food,".vhdl")+5]
-						logfile.write(cmd+"\n")
-						print(cmd)
-		 				status=commands.getoutput(cmd)
-		 				if(status):
-							print "ghdl -a error:"
-							print status
-							logfile.write(status+"\n")
-							did_compile=False
-							pass_test=False
-
-						cmd=ghdl_food[string.find(ghdl_food,"ghdl -e"):string.find(ghdl_food,"   ghdl -r")-1]
-						logfile.write(cmd+"\n")
-						print(cmd)
-		 				status=commands.getoutput(cmd)
-		 				if(status):
-							print "ghdl -e error:"
-							print status
-							logfile.write(status+"\n")
-							pass_test=False
-							did_compile=False
-
-						cmd=ghdl_food[string.find(ghdl_food,"ghdl -r"):string.find(ghdl_food,".vcd")+4]
-						logfile.write(cmd+"\n")
-						print(cmd)
-		 				status=commands.getoutput(cmd)
-		 				if string.find(status, "Incorrect") !=-1:
-							print "ghdl -r error:"
-							print status
-							logfile.write(status+"\n")
-							pass_test=False
-						commands.getoutput("rm *.vcd e~testbench* testbench*")
-
-				pass_test = pass_test and did_generate_vhdl
-				res.append( [run_cmd, `did_generate_vhdl`, `did_compile`, `pass_test`])
-				pass_all = pass_all and pass_test
+									#did_compile = False
 		
-				if pass_test:
-					print "Test: ", run_cmd , " has SUCCEDED "
-				else:
-					print "Test: ", run_cmd , " has FAILED "
+									status = string.find(st, "Error loading design")
+									if status > 0:
+										did_compile = False
+										finished = True
 
+							if did_compile:
+								child_stdin.write('exit \n')
+								child_stdout.close()
+								child_stdin.close()
+								child_stderr.close()
 
-		fd.close()
+						else: # use ghdl
 
+							cmd=ghdl_food[string.find(ghdl_food,"ghdl -a"):string.find(ghdl_food,".vhdl")+5]
+							logfile.write(cmd+"\n")
+							print(cmd)
+			 				status=commands.getoutput(cmd)
+			 				if(status):
+								print "ghdl -a error:"
+								print status
+								logfile.write(status+"\n")
+								did_compile=False
+								pass_test=False
 
-printResults( res , logfile)
+							cmd=ghdl_food[string.find(ghdl_food,"ghdl -e"):string.find(ghdl_food,"   ghdl -r")-1]
+							logfile.write(cmd+"\n")
+							print(cmd)
+			 				status=commands.getoutput(cmd)
+			 				if(status):
+								print "ghdl -e error:"
+								print status
+								logfile.write(status+"\n")
+								pass_test=False
+								did_compile=False
 
-print "FINAL PASS STATUS: ", `pass_all`
-logfile.write("FINAL PASS STATUS:" +  `pass_all`)
+							cmd=ghdl_food[string.find(ghdl_food,"ghdl -r"):string.find(ghdl_food,".vcd")+4]
+							logfile.write(cmd+"\n")
+							print(cmd)
+			 				status=commands.getoutput(cmd)
+			 				if string.find(status, "Incorrect") !=-1:
+								print "ghdl -r error:"
+								print status
+								logfile.write(status+"\n")
+								pass_test=False
+							commands.getoutput("rm *.vcd e~testbench* testbench*")
 
-logfile.close()
-print "Logfile was written to: release_test.log"
+					pass_test = pass_test and did_generate_vhdl
+					res.append([run_cmd, `did_generate_vhdl`, `did_compile`, `pass_test`])
+					
+					res_line = run_cmd + ", " + str(did_generate_vhdl) + ", " + str(did_compile) + ", " + str(pass_test) + "\n"
+					resultfile.write( res_line )
+					resultfile.close()
+					pass_all = pass_all and pass_test
+		
+					if pass_test:
+						print "Test: ", run_cmd , " has SUCCEDED "
+					else:
+						print "Test: ", run_cmd , " has FAILED "
+
+			fd.close()
+			
+			if (completedFile == False):
+				pre_append( "@", "tests/"+filename); 
+
+	printResults( res , logfile)
+
+	print "FINAL PASS STATUS: ", `pass_all`
+	logfile.write("FINAL PASS STATUS:" +  `pass_all`)
+
+	logfile.close()
+	print "Logfile was written to: release_test.log"
 
