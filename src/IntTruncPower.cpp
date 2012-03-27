4f68b079afd00ea75fa2967d97f8a2a7689e1ab9
Guillaume Sergent <no@available>
2012-03-27 02:36:36 +0000

    add new files with preliminary work on implementing optimized multiplication tables for a fixed-point/integer truncated power-of-n component

---
+++ src/IntTruncPower.cpp
@@ -0,0 +1,99 @@
+#include "IntTruncPower.hpp"
+MonomialOfBits MonomialOfBits::operator* (const MonomialOfBits& rhs) const
+{
+	if (this->n != rhs.n)
+		throw "sizes don't match (MonomialOfBits::operator+)";
+	MonomialOfBits res (*this);
+	std::vector<bool>::iterator i = res.data.begin();
+	std::vector<bool>::const_iterator j = rhs.data.begin();
+	for (;j != rhs.data.end (); i++,j++) {
+		*i = *i || *j;
+	}
+	return res;
+}
+// too long a typename
+using std::tr1::unordered_map;
+ProductBitIR::ProductBitIR (const ProductBit& rhs)
+	:data (unordered_map<MonomialOfBits,int>())
+{
+	std::list<MonomialOfBits>::const_iterator it
+		= rhs.data.begin();
+	for (; it != rhs.data.end(); it++) {
+		data[*it] = getTimes (*it) + 1;
+	}
+}
+int ProductBitIR::getTimes (const MonomialOfBits& e) const
+{
+	unordered_map<MonomialOfBits, int>::const_iterator it
+		= data.find (e);
+	if (it == data.end()) {
+		return 0;
+	}
+	return it->second;
+}
+ProductBitIR& ProductBitIR::operator+= (const ProductBitIR& rhs)
+{
+	unordered_map<MonomialOfBits, int>::const_iterator it
+		= rhs.data.begin();
+	for (; it != rhs.data.end(); it++) {
+		this->data[it->first] =
+			this->getTimes(it->first) + it->second;
+	}
+	return *this;
+}
+ProductBitIR ProductBitIR::operator* (const ProductBitIR& rhs)
+{
+	ProductBitIR res;
+	unordered_map<MonomialOfBits, int>::const_iterator i
+		= this->data.begin();
+	unordered_map<MonomialOfBits, int>::const_iterator j
+		= res.data.begin();
+	for (; i != this->data.end(); i++) {
+		for (; j != res.data.end(); j++) {
+			MonomialOfBits prod = i->first * j->first;
+			res.data[prod] =
+				res.getTimes(prod) + (i->second * j->second);
+		}
+	}
+	return res;
+}
+ProductIR& ProductIR::operator+= (const ProductIR& rhs)
+{
+	if (this->data.size() != rhs.data.size())
+		throw "size mismatch (ProductIR::operator+=)";
+	if (this->msb != rhs.msb)
+		throw "msb mismatch (ProductIR::operator+=)";
+	std::vector<ProductBitIR>::const_iterator j = rhs.data.begin();
+	std::vector<ProductBitIR>::iterator i = this->data.begin();
+	for (; j != rhs.data.end(); i++,j++) {
+		*i += *j;
+	}
+	return *this;
+}
+ProductIR& ProductIR::operator>>= (int n)
+{
+	if (n < 0)
+		throw "invalid argument (ProductIR::operator>>=)";
+	std::vector<ProductBitIR>::iterator i = this->data.begin();
+	std::vector<ProductBitIR>::iterator j = i + n;
+	for (; j != this->data.end(); i++,j++) {
+		*i = *j;
+	}
+	i = this->data.begin();
+	j = i+n;
+	for (; i != j; i++) {
+		*i = ProductBitIR();
+	}
+	return *this;
+}
+//incomplete
+ProductIR ProductIR::operator* (const ProductIR& rhs)
+{
+	size_t n = this->data.size(), m = rhs.data.size();
+	ProductIR res (n+m, this->msb + rhs.msb);
+	ProductIR tmp (n+m, this->msb + rhs.msb);
+	for (int i = 0; i < n; i++) {
+	}
+	return res;
+}
+
---
+++ src/IntTruncPower.hpp
@@ -0,0 +1,75 @@
+//#include "Operator.hpp"
+//#include "utils.hpp"
+#include <vector>
+#include <list>
+#include <tr1/unordered_map>
+
+//using namespace flopoco;
+
+/* some bit of context here:
+ * there's b, a polynomial which is written b_1*1b-1 + b_2*1b-2 + ... +
+ * b_n * 1b-n
+ * and we try to calculate a power p of b, by evaluating the full
+ * formula the simplifying it (see Jérémie Detrey's thesis pp. 40-41)
+ */
+
+class MonomialOfBits {
+	public:
+		MonomialOfBits (size_t n): data(std::vector<bool>(n)) {
+		}
+		MonomialOfBits (const MonomialOfBits& m) :n(m.n) {
+			this->data = m.data;
+		}
+		MonomialOfBits operator* (const MonomialOfBits& rhs) const;
+		MonomialOfBits operator= (const MonomialOfBits& rhs) {
+			this->n = rhs.n;
+			this->data = rhs.data;
+		}
+		bool operator== (const MonomialOfBits& rhs) const {
+			return (this->n == rhs.n) && (this->data == rhs.data);
+		}
+		std::vector<bool> data;
+		size_t n;
+};
+
+class ProductBit;
+/*
+struct ProductBitIRElt {
+	int times;
+	ProductBit data;
+};
+*/
+class ProductBitIR;
+class ProductBit {
+	public:
+		ProductBit (): data (std::list<MonomialOfBits>()) {
+		}
+		std::list<MonomialOfBits> data;
+};
+
+class ProductBitIR {
+	public:
+		ProductBitIR (const ProductBit& rhs);
+		ProductBitIR ()
+			:data (std::tr1::unordered_map<MonomialOfBits,int>()) {
+		}
+		int getTimes (const MonomialOfBits& e) const;
+		ProductBitIR& operator+= (const ProductBitIR& rhs);
+		ProductBitIR operator* (const ProductBitIR& rhs);
+		std::tr1::unordered_map<MonomialOfBits, int> data;
+};
+
+class ProductIR {
+	public:
+		ProductIR (size_t w, int msb) 
+			:data (std::vector<ProductBitIR> (w, ProductBitIR()))
+			,msb (msb) {
+		}
+		ProductIR& operator+= (const ProductIR& rhs);
+		ProductIR& operator>>= (int n);
+		ProductIR& operator*= (const ProductBitIR& rhs);
+		ProductIR operator* (const ProductIR& rhs);
+		std::vector<ProductBitIR> data;
+		int msb;
+};
+
