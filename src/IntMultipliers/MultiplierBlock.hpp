e94371aabf213f1c88be949c50b9a03f021e268a
Kinga Illyes <no@available>
2012-08-02 14:56:17 +0000

    some comments

--- src/IntMultipliers/MultiplierBlock.hpp
+++ src/IntMultipliers/MultiplierBlock.hpp
@@ -36,64 +36,113 @@ namespace flopoco {
 	
 		
 		/**
-		 * Set the value of signalName
-		 * @param the new value
+		 * Set the value of the output signal name
+		 * @param name= the new value
 		 */
 		void setSignalName(string name);
 
+		/**
+		 * Set the value of the output signal length
+		 * @param length= the new length
+		 */
 		void setSignalLength(int length);
 
+		/**
+		*returns the weight of the multiplierBlock (top right corner)		
+		**/
 		int getWeight()
 		{
 			return weight;
 		}
 
+
+		/**
+		*returns the output signal name
+		**/
 		string getSigName();
 
+		/**
+		*returns the output signal length
+		**/
 		int getSigLength();
 
+		/**
+		*returns the width of the DSP
+		**/
 		int getwX();
 
+		/**
+		*returns the height of the DSP
+		**/
 		int getwY();
 
+		/**
+		*returns the top right corner X coordinate
+		**/
 		int gettopX();
 
+		/**
+		*returns the top right corner Y coordinate
+		**/
 		int gettopY();
 
+		/**
+		*returns the bottom left corner X coordinate
+		**/
 		int getbotX();
 		
+		/**
+		*returns the bottom left corner Y coordinate
+		**/
 		int getbotY();
 
 		int getCycle();
 
+
+		/**
+		*returns the name of the first signal which enters in DSP. 
+		*for example if the DSP is doing a multiplication like Z <=X * Y
+		*then X is the first input signal and Y is the second output signal
+		**/
 		string getInputName1()
 		{return inputName1;}
 
-		
+		/**
+		*returns the name of the second  signal which enters in DSP. 
+		*for example if the DSP is doing a multiplication like Z <=X * Y
+		*then X is the first input signal and Y is the second output signal
+		**/
 		string getInputName2()
 		{return inputName2;}
 		
+		/**
+		*verifies if two block can be chained in the same supertile or not
+		**/
 		bool canBeChained(MultiplierBlock* next);
 
+		/**
+		*returns the next block which is chained with the current block in the supertile
+		**/
 		MultiplierBlock* getNext();
 
+		/**
+		*returns the previous block which is chained with the current block in the supertile
+		**/
 		MultiplierBlock* getPrevious();
 
+		/**
+		*sets the next block which is chained with the current block in the supertile
+		**/
 		void setNext(MultiplierBlock* b);
 
+		/**
+		*sets the previous block which is chained with the current block in the supertile
+		**/
 		void setPrevious(MultiplierBlock* b);
 		
-		string PP(int i, int j, int nr=-1);
-		string PPTbl( int i, int j, int nr=-1);
-		string XY(int i, int j, int nr=-1);
-		string heap( int i, int j);
+			
 
-		void generateVHDLforDSP(int nr,int i);
-		void generateVHDLforLOGIC(int nr);
-		void generateVHDL(int nr, int i);
-	
-
-	bool operator <= (MultiplierBlock* b); 
+		bool operator <= (MultiplierBlock* b); 
 
 		
 	private:
@@ -105,11 +154,11 @@ namespace flopoco {
 		int cycle;							/**< cycle */
 		MultiplierBlock* previous;
 		MultiplierBlock* next;
-		string signalName;
-		int signalLength;
+		string signalName;					/**<the name of the output signal*/
+		int signalLength;					/**<the length of the output signal*/
 		int weight;
-		int weightShift;
-		string inputName1, inputName2;
+		int weightShift;					/**<wFull-wOut-g*/
+		string inputName1, inputName2;		/**<names of the inputs*/
 	};
 
 }
