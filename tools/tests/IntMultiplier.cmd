3980d95a926164cf62560d0ef79274083917f20a
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2012-08-20 09:27:27 +0000

    new interface

--- tools/tests/IntMultiplier.cmd
+++ tools/tests/IntMultiplier.cmd
@@ -1,24 +1,25 @@
 @
 #IntMultiplier
-flopoco -target=Virtex5  IntMultiplier 2 2 0 0   1
-flopoco -target=Virtex5  IntMultiplier 2 2 0 1   1
-flopoco -target=Virtex5  IntMultiplier 4 3 0 0   1
-flopoco -target=Virtex5  IntMultiplier 3 4 0 1   1
-flopoco -target=Virtex5  IntMultiplier 2 2 3 0   1
+flopoco -target=Virtex5  IntMultiplier 2 2 4 0   1
+flopoco -target=Virtex5  IntMultiplier 2 2 4 1   1
+flopoco -target=Virtex5  IntMultiplier 4 3 7 0   1
+flopoco -target=Virtex5  IntMultiplier 3 4 7 1   1
+flopoco -target=Virtex5  IntMultiplier 2 2 2 0   1
+flopoco -target=Virtex5  IntMultiplier 2 2 2 1   1
 flopoco -target=Virtex5  IntMultiplier 4 3 4 0   1
 flopoco -target=Virtex5  IntMultiplier 3 4 4 1   1
 
-flopoco -target=Virtex5  IntMultiplier 8 8 0  0   1
-flopoco -target=Virtex5  IntMultiplier 8 8 0  1   1
-flopoco -target=Virtex5  IntMultiplier 8 8 8  0   1
-flopoco -target=Virtex5  IntMultiplier 8 8 8  1   1
+flopoco -target=Virtex5  IntMultiplier 8 8 16  0   1
+flopoco -target=Virtex5  IntMultiplier 8 8 16  1   1
+flopoco -target=Virtex5  IntMultiplier 8 8 8   0   1
+flopoco -target=Virtex5  IntMultiplier 8 8 8   1   1
 
-flopoco -target=Virtex5  IntMultiplier 8 10 0   0   1
-flopoco -target=Virtex5  IntMultiplier 8 10 0   1   1
+flopoco -target=Virtex5  IntMultiplier 8 10 18  0   1
+flopoco -target=Virtex5  IntMultiplier 8 10 18  1   1
 flopoco -target=Virtex5  IntMultiplier 8 10 10  0   1
 flopoco -target=Virtex5  IntMultiplier 8 10 10  1   1
 
-flopoco -target=Virtex5  IntMultiplier 2 10 0   0   1
-flopoco -target=Virtex5  IntMultiplier 2 10 10  0   1
-flopoco -target=Virtex5  IntMultiplier 2 10 0   1   1
-flopoco -target=Virtex5  IntMultiplier 2 10 10  1   1
+flopoco -target=Virtex5  IntMultiplier 2 10 12   0   1
+flopoco -target=Virtex5  IntMultiplier 2 10 10   0   1
+flopoco -target=Virtex5  IntMultiplier 2 10 12   1   1
+flopoco -target=Virtex5  IntMultiplier 2 10 10   1   1
