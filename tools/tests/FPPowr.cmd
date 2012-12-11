bd64ffcca58ee4f59094cda287d1dcaff078d98a
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2012-12-11 11:48:57 +0000

    fppow

--- tools/tests/FPPowr.cmd
+++ tools/tests/FPPowr.cmd
@@ -1,3 +1,7 @@
 #   FPPowr wE wF
-flopoco FPExp 8 23
-flopoco FPExp 11 52
+flopoco -target=Virtex5 FPPow  8 23
+flopoco -target=Virtex5 FPPow  11 52
+flopoco -target=StratixV FPPow 8 23
+flopoco  -target=StratixV  FPPow 11 52
+flopoco -target=StratixV FPPowr 8 23
+flopoco  -target=StratixV  FPPowr 11 52
