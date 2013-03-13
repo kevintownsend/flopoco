abe88e60d83c265f9dbc0f477141a853b02336bf
Fabrizio Ferrandi <fabrizio.ferrandi@polimi.it>
2013-03-13 10:01:23 +0000

    * Added a test that shows a bug on FPLog. The message returned with 'flopoco -frequency=100 -target=Virtex5 FPLog 8 23 0' is: "Exception while parsing command line: active cycle of signal S1 is later than current cycle, cannot delay it".

--- tools/tests/FPLog.cmd
+++ tools/tests/FPLog.cmd
@@ -6,3 +6,5 @@ flopoco -target=StratixIV FPLog 11 52 0
 flopoco -target=Virtex5 FPLog 8 23 0
 flopoco -target=Virtex5 FPLog 11 28 0
 flopoco -target=Virtex5 FPLog 11 52 0
+flopoco -frequency=100 -target=Virtex5 FPLog 8 23 0
+
