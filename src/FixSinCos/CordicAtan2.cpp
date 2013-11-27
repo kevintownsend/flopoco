9bd1af1a4fe5bfcb048def3d5af5288c44c87263
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2013-11-27 14:30:46 +0000

    more Atan2

--- src/FixSinCos/CordicAtan2.cpp
+++ src/FixSinCos/CordicAtan2.cpp
@@ -10,10 +10,10 @@ using namespace std;
 
 namespace flopoco{
 
+	// TODO Options:
+	// An option to output the angle in the (atan(2^-i)) basis
+	// an an option for outputting the norm of the vector as well (scaled or not)
 
-	//The wIn+1 below is for consistency with FixSinCos and FixSinOrCos interfaces.
-	// TODO possibly fix all the code instead... This would enable sharing emulate() etc.
- 
 	CordicAtan2::CordicAtan2(Target* target, int w_, map<string, double> inputDelays) 
 		: Operator(target), w(w_)
 	{
@@ -39,12 +39,15 @@ namespace flopoco{
 		REPORT(DEBUG, "Using rounded rotation trick");
 #endif
 
+		// ulp = weight of the LSB of the result is 2^(-w+1)
+		// half-ulp is 2^-w
+		// atan(2^-w) < 2^-w therefore after w interations,  method error will be bounded by 2^-w 
+		maxIterations = w;
 
-
-#if 0
 		//error analysis
 		double eps;  //error in ulp
-		eps=0.5; //initial rounding of kfactor
+		eps=2; // initial neg-by-not
+
 		double shift=0.5;
 		for(stage=1; stage<=maxIterations; stage++){
 #if ROUNDED_ROTATION
@@ -55,16 +58,11 @@ namespace flopoco{
 			shift *=0.5;
 		}
 
-		if (reducedIterations == 1) {
-			eps+=2; // two multiplications in sequence, each truncated
-		}
-
 		eps+=1; // the final neg-by-not
-		REPORT(DEBUG, "Error analysis computes eps=" << eps << " ulps (before final rounding)");
 
 		// guard bits depend only on the number of iterations
 		g = 1+(int) ceil(log2(eps)); // +1 for the final rounding 
-#endif
+		REPORT(DEBUG, "Error analysis computes eps=" << eps << " ulps (before final rounding), hence  g=" << g);
 		
 		// *********    internal precision and fixed-point alignment **************
 
@@ -99,9 +97,127 @@ namespace flopoco{
 		addOutput  ( "A"  , w, 2 );
 		
 		setCriticalPath(getMaxInputDelays(inputDelays));
-		manageCriticalPath( target->lutDelay());
+		//		manageCriticalPath( target->lutDelay());
+
+
+
+
+		///////////// VHDL GENERATION
+
+		// manage symmetries: we need to reduce (X,Y) to a quadrant (-pi/4, pi/4)
+
+		vhdl << tab << declare("sgnX") << " <= X" << of(w-1) << ";" << endl;
+		vhdl << tab << declare("sgnY") << " <= X" << of(w-1) << ";" << endl;
+
+		// TODO: replace the following with LUT-based comparators
+		// and first maybe experiment with synthesis tools
+		vhdl << tab << declare("XmY", w+1) << " <= (sgnX & X)-(sgnY & Y);" << endl;
+		vhdl << tab << declare("XpY", w+1) << " <= (sgnX & X)+(sgnY & Y);" << endl;
+ 		vhdl << tab << declare("XltY") << " <= XmY" << of(w) <<";" << endl;
+		vhdl << tab << declare("mYltX") << " <= XpY" << of(w) <<";" << endl;
+
+		vhdl << tab << "-- quadrant will also be the angle to add at the end" <<endl;
+		vhdl << tab << declare("quadrant", 2) << " <= " << endl;
+		vhdl << tab << tab << "\"00\"  when (not sgnX and not XltY and     mYltX)='1') else"    << endl;
+		vhdl << tab << tab << "\"01\"  when (not sgnY and     XltY and     mYltX)='1') else"    << endl;
+		vhdl << tab << tab << "\"10\"  when (    sgnX and     XltY and not mYltX)='1') else"    << endl;
+		vhdl << tab << tab << "\"11\"  when others;"    << endl;
+
+		vhdl << tab << declare("mX", w+g) << " <= not (X&" << zg(g)<< ");  -- negation by not, implies one ulp error." << endl;
+		vhdl << tab << declare("mY", w+g) << " <= not (Y&" << zg(g)<< ");  -- negation by not, implies one ulp error. " << endl;
+
+		vhdl << tab << declare("X1", w+g) << " <= " << endl;
+		vhdl << tab << tab << " X when quadrant=\"00\"   else " << endl;
+		vhdl << tab << tab << " Y when quadrant=\"01\"   else " << endl;
+		vhdl << tab << tab << "mX when quadrant=\"10\"   else " << endl;
+		vhdl << tab << tab << "mY  when others;"    << endl;
+
+		vhdl << tab << declare("Y1", w+g) << " <= " << endl;
+		vhdl << tab << tab << " Y when quadrant=\"00\"   else " << endl;
+		vhdl << tab << tab << "mX when quadrant=\"01\"   else " << endl;
+		vhdl << tab << tab << "mY when quadrant=\"10\"   else " << endl;
+		vhdl << tab << tab << " X  when others;"    << endl;
+
+
+		//CORDIC iterations
+
+		for(stage=1; stage<=maxIterations; stage++){
+			//shift Xin and Yin with 2^n positions to the right
+			// From there on X is always positive, but Y may be negative and thus need sign extend
+			vhdl << tab << declare(join("XShift", stage), w+g) << " <= " << zg(stage) << " & X" << stage << range(w+g-1, stage) << ";" <<endl;
+		 
+			vhdl << tab << declare(join("sgnY", stage))  << " <= " <<  join("Y", stage)  <<  of(w+g-1) << ";" << endl; 
+			vhdl << tab << declare(join("YShift", stage), w+g) 
+			     << " <= " << rangeAssign(w+g-1, w+g-stage, join("sgnY", stage))   
+			     << " & Y" << stage << range(w+g, stage) << ";" << endl;
+			
+			// Critical path delay for one stage:
+			// The data dependency is from one Z to the next 
+			// We may assume that the rotations themselves overlap once the DI are known
+			//manageCriticalPath(target->localWireDelay(w) + target->adderDelay(w) + target->lutDelay()));
+			// 			manageCriticalPath(target->localWireDelay(sizeZ) + target->adderDelay(sizeZ));
+			
+
+#if 0
+
+#if ROUNDED_ROTATION  // rounding of the shifted operand, should save 1 bit in each addition
+			vhdl << tab << declare(join("XShiftRoundBit", stage)) << " <= " << join("X", stage)  << of(stage-1) << ";" << endl;
+			vhdl << tab << declare(join("YShiftRoundBit", stage)) << " <= " << join("Y", stage) << of(stage-1) << ";" <<endl;
+			vhdl << tab << declare(join("XShiftNeg", stage), w+1) << " <= " << rangeAssign(w, 0, join("D", stage)) << " xor " << join("XShift", stage)   << " ;" << endl;
+			vhdl << tab << declare(join("YShiftNeg", stage), w+1) << " <= (not " << rangeAssign(w, 0, join("D", stage)) << ") xor " << join("YShift", stage)   << " ;" << endl;
+
+			vhdl << tab << declare(join("X", stage+1), w+1) << " <= " 
+			     << join("X", stage) << " + " << join("YShiftNeg", stage) << " +  not (" << join("D", stage) << " xor " << join("YShiftRoundBit", stage) << ") ;" << endl;
+
+			vhdl << tab << declare(join("Y", stage+1), w+1) << " <= " 
+			     << join("Y", stage) << " + " << join("XShiftNeg", stage) << " + (" << join("D", stage) << " xor " << join("XShiftRoundBit", stage) << ") ;" << endl;
+
+#else
+// truncation of the shifted operand
+			vhdl << tab << declare(join("X", stage+1), w+1) << " <= " 
+			     << join("X", stage) << " - " << join("YShift", stage) << " when " << join("D", stage) << "=\'0\' else "
+			     << join("X", stage) << " + " << join("YShift", stage) << " ;" << endl;
+
+			vhdl << tab << declare(join("Y", stage+1), w+1) << " <= " 
+			     << join("Y", stage) << " + " << join("XShift", stage) << " when " << join("D", stage) << "=\'0\' else "
+			     << join("Y", stage) << " - " << join("XShift", stage) << " ;" << endl;
+#endif			
+			
+			//create the constant signal for the arctan
+			mpfr_init2(zatan, 10*w);
+			
+			mpfr_set_d(zatan, 1.0, GMP_RNDN);
+			mpfr_div_2si(zatan, zatan, stage, GMP_RNDN);
+			mpfr_atan(zatan, zatan, GMP_RNDN);
+			mpfr_div(zatan, zatan, constPi, GMP_RNDN);
+			REPORT(DEBUG, "stage=" << stage << "  atancst=" << printMPFR(zatan, 15));		
+			//create the arctangent factor to be added to Zin
+									
+			REPORT(DEBUG, "  sizeZ=" << sizeZ << "   zMSB="<<zMSB );
+
+			if(stage<maxIterations || reducedIterations == 1) {
+				// LSB is always -w 
+				vhdl << tab << declare(join("atan2PowStage", stage), sizeZ) << " <= " << unsignedFixPointNumber(zatan, zMSB, zLSB) << ";" <<endl;
+				
+				vhdl << tab << declare(join("fullZ", stage+1), sizeZ) << " <= " 
+				     << join("Z", stage) << " + " << join("atan2PowStage", stage) << " when " << join("D", stage) << "=\'1\' else "
+				     << join("Z", stage) << " - " << join("atan2PowStage", stage) << " ;" << endl;
+				vhdl << tab << declare(join("Z", stage+1), sizeZ-1) << " <= "<< join("fullZ", stage+1) << range(sizeZ-2, 0) << ";" << endl; 
+				vhdl << tab << declare(join("D", (stage+1))) << " <= fullZ" << stage+1 << "(" << sizeZ-1 <<");" <<endl;
+			}
+			//decrement the size of Z
+			sizeZ--;
+			zMSB--;
+		}
 		
-	};
+		// Give the time to finish the last rotation
+		manageCriticalPath( target->localWireDelay(w+1) + target->adderDelay(w+1) // actual CP delay
+		                    - (target->localWireDelay(sizeZ+1) + target->adderDelay(sizeZ+1))); // CP delay that was already added
+
+#endif
+		// reconstruction
+	}		
+};
 
 
 	CordicAtan2::~CordicAtan2(){
