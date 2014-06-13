068e14dd68c8907f4b51d32497a257a6c42a512f
Florent de Dinechin <Florent.de-Dinechin@insa-lyon.fr>
2014-06-13 15:07:40 +0000

    oops

--- src/main.hpp
+++ src/main.hpp
@@ -1,7 +1,6 @@
 #define BRIGHT 1
 #define RED 31
-#define OPER 32
-#define NEWOPER 32
+#define OPER 35
 #define PARAM 34
-#define OP(op,paramList)             {cerr << "    "; printf("%c[%d;%dm",27,1,OPER); cerr <<  op; printf("%c[%dm",27,0); cerr<< " "; printf("%c[%d;%dm",27,1,PARAM); cerr << paramList; printf("%c[%dm\n",27,0); } 
-#define NEWOP(op,paramList)          {cerr << "    "; printf("%c[%d;%dm",27,1,NEWOPER); cerr <<  op; printf("%c[%dm",27,0); cerr<< " "; printf("%c[%d;%dm",27,1,PARAM); cerr << paramList; printf("%c[%dm\n",27,0); } 
+#define OP(op,paramList)   {printf("%c[%d;%dm",27,1,OPER); cerr <<  op; printf("%c[%dm",27,0); cerr<< " "; printf("%c[%d;%dm",27,1,PARAM); cerr << paramList; printf("%c[%dm\n",27,0); } 
+
