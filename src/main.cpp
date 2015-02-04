70d4e7cf1d9815819c6e1ed80a4e894dfac90f33
Matei Istoan <matei000@yahoo.com>
2015-02-04 10:10:20 +0000

    stop the simulation without an error for operators tested using TestBench, not just with TestBenchFile

--- src/main.cpp
+++ src/main.cpp
@@ -1722,7 +1722,7 @@ else if(opname=="IntAdder"){
 #endif
 			cerr <<  "ghdl -a " << simlibs << "-fexplicit "<< filename <<endl;
 			cerr <<  "ghdl -e " << simlibs << "-fexplicit " << op->getName() <<endl;
-			cerr <<  "ghdl -r " << simlibs << op->getName() << " --vcd=" << op->getName() << ".vcd" <<endl;
+			cerr <<  "ghdl -r " << simlibs << op->getName() << " --vcd=" << op->getName() << ".vcd --stop-time=" << ((TestBench*)op)->getSimulationTime() << "ns" <<endl;
 			cerr <<  "gtkwave " << op->getName() << ".vcd" << endl;
 		}
 		
